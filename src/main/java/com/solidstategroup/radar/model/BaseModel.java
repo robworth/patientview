0b7ebb68eff48becf59888e95c53e20050382b8f
David Hemmings <davidh@solidstategroup.com>
2011-12-22 12:33:17 +0000

    Added Diagnosis panel and relevant model objects

---
+++ src/main/java/com/solidstategroup/radar/model/BaseModel.java
@@ -0,0 +1,9 @@
+package com.solidstategroup.radar.model;
+
+import java.io.Serializable;
+
+public class BaseModel implements Serializable {
+
+    private Long id;
+
+}
---
+++ src/main/java/com/solidstategroup/radar/model/DiagnosisCode.java
@@ -0,0 +1,23 @@
+package com.solidstategroup.radar.model;
+
+public class DiagnosisCode extends BaseModel {
+
+    private String description;
+    private String abbreviation;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+}
