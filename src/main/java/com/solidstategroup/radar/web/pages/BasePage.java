f2f1c70c3517f263ff61dec577d40119d89411ed
David Hemmings <davidh@solidstategroup.com>
2011-12-22 14:23:18 +0000

    Fixed compile

--- src/main/java/com/solidstategroup/radar/web/pages/BasePage.java
+++ src/main/java/com/solidstategroup/radar/web/pages/BasePage.java
@@ -1,8 +1,5 @@
 package com.solidstategroup.radar.web.pages;
 
-
-
-import com.solidstategroup.radar.web.pages.newPatient.DemographicsPage;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
@@ -13,17 +10,20 @@ public abstract class BasePage extends WebPage {
         add(new Label("title", getTitle()));
 
         BookmarkablePageLink[] naviationPages = {
-            new BookmarkablePageLink("homePage", HomePage.class),
-            new BookmarkablePageLink("proRegistrationPage", RegistrationProfessionalPage.class),
-            new BookmarkablePageLink("professionalsPage", ProfessionalsPage.class),
-            new BookmarkablePageLink("patientsPage", PatientsPage.class),
-            new BookmarkablePageLink("diseaseIndexPage", DiseaseIndexPage.class),
-            new BookmarkablePageLink("demographicsPage", DemographicsPage.class),
-            new BookmarkablePageLink("patientsListingPage", PatientsListingPage.class),
-            new BookmarkablePageLink("recruitmentPage", RecruitmentPage.class),
-            new BookmarkablePageLink("mpgnPage", MpgnPage.class),
-            new BookmarkablePageLink("srnsPage", SrnsPage.class),
-            new BookmarkablePageLink("consentFormsPage", ConsentFormsPage.class)
+                new BookmarkablePageLink("homePage", HomePage.class),
+                new BookmarkablePageLink("proRegistrationPage", RegistrationProfessionalPage.class),
+                new BookmarkablePageLink("professionalsPage", ProfessionalsPage.class),
+                new BookmarkablePageLink("patientsPage", PatientPage.class),
+                new BookmarkablePageLink("diseaseIndexPage", DiseaseIndexPage.class),
+
+                // Todo: This is wrong for now
+                new BookmarkablePageLink("demographicsPage", PatientPage.class),
+
+                new BookmarkablePageLink("patientsListingPage", PatientsListingPage.class),
+                new BookmarkablePageLink("recruitmentPage", RecruitmentPage.class),
+                new BookmarkablePageLink("mpgnPage", MpgnPage.class),
+                new BookmarkablePageLink("srnsPage", SrnsPage.class),
+                new BookmarkablePageLink("consentFormsPage", ConsentFormsPage.class)
         };
 
         add(naviationPages);
