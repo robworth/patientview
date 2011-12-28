de8fe118d180e1fe64ae8eaea76ce8d0b4012e4e
David Hemmings <davidh@solidstategroup.com>
2011-12-28 12:44:49 +0000

    Added family history to diagnosis page

---
+++ src/main/java/com/solidstategroup/radar/web/panels/PatientDiagnosisRelativePanel.java
@@ -0,0 +1,20 @@
+package com.solidstategroup.radar.web.panels;
+
+import com.solidstategroup.radar.model.Relative;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.ComponentPropertyModel;
+
+import java.util.Collections;
+
+public class PatientDiagnosisRelativePanel extends Panel {
+
+    public PatientDiagnosisRelativePanel(String id, int i) {
+        super(id);
+        add(new DropDownChoice<Relative>("relative", new ComponentPropertyModel("relativeWithDiseaseRadarNumber" + i),
+                Collections.<Relative>emptyList()));
+        add(new TextField("radarNumber", new ComponentPropertyModel("relativeWithDisease" + i)));
+    }
+
+}
