ab1ae6e5946fd40d2fe3191bacba3ed89df2d06c
David Hemmings <davidh@solidstategroup.com>
2011-12-30 15:57:58 +0000

    Made utility class abstract

--- src/main/java/com/solidstategroup/radar/model/BaseModel.java
+++ src/main/java/com/solidstategroup/radar/model/BaseModel.java
@@ -2,8 +2,15 @@ package com.solidstategroup.radar.model;
 
 import java.io.Serializable;
 
-public class BaseModel implements Serializable {
+public abstract class BaseModel implements Serializable {
 
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
