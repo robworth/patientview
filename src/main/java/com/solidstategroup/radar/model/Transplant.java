5f115b0b6c37667d5ea57d3c79697331d7f19369
David Hemmings <davidh@solidstategroup.com>
2011-12-30 15:38:29 +0000

    Added getters and setters

--- src/main/java/com/solidstategroup/radar/model/Transplant.java
+++ src/main/java/com/solidstategroup/radar/model/Transplant.java
@@ -11,4 +11,67 @@ public class Transplant extends BaseModel {
     private Boolean recurr;
     private Date dateRecurr, dateFailure, dateRejected, dateBiopsy;
 
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public Boolean getRecurr() {
+        return recurr;
+    }
+
+    public void setRecurr(Boolean recurr) {
+        this.recurr = recurr;
+    }
+
+    public Date getDateRecurr() {
+        return dateRecurr;
+    }
+
+    public void setDateRecurr(Date dateRecurr) {
+        this.dateRecurr = dateRecurr;
+    }
+
+    public Date getDateFailure() {
+        return dateFailure;
+    }
+
+    public void setDateFailure(Date dateFailure) {
+        this.dateFailure = dateFailure;
+    }
+
+    public Date getDateRejected() {
+        return dateRejected;
+    }
+
+    public void setDateRejected(Date dateRejected) {
+        this.dateRejected = dateRejected;
+    }
+
+    public Date getDateBiopsy() {
+        return dateBiopsy;
+    }
+
+    public void setDateBiopsy(Date dateBiopsy) {
+        this.dateBiopsy = dateBiopsy;
+    }
 }
