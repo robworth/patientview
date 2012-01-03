84134aeef60d9a938ec5b4fb3d9b76d9d85c7ee7
David Hemmings <davidh@solidstategroup.com>
2012-01-03 18:40:08 +0000

    DBUnit integration with DAO tests

--- src/test/java/com/solidstategroup/radar/test/dao/BaseDaoTest.java
+++ src/test/java/com/solidstategroup/radar/test/dao/BaseDaoTest.java
@@ -1,6 +1,12 @@
 package com.solidstategroup.radar.test.dao;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseDataSourceConnection;
+import org.dbunit.dataset.xml.XmlDataSet;
+import org.dbunit.ext.h2.H2DataTypeFactory;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -11,6 +17,7 @@ import org.springframework.test.context.ContextConfiguration;
 import javax.sql.DataSource;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 @RunWith(org.springframework.test.context.junit4.SpringJUnit4ClassRunner.class)
@@ -18,7 +25,10 @@ import java.sql.Statement;
 public abstract class BaseDaoTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseDaoTest.class);
-    private static final String CREATE_TABLES_SQL = "create_tables.sql";
+
+    // Filenames
+    private static final String CREATE_TABLES_SQL_FILENAME = "create_tables.sql";
+    private static final String DATASET_XML_FILENAME = "dataset.xml";
 
     @Autowired
     private DataSource dataSource;
@@ -33,23 +43,28 @@ public abstract class BaseDaoTest {
             connection = dataSource.getConnection();
             statement = connection.createStatement();
 
-            // Load the statements and execute each one
-            InputStream inputStream =
-                    Thread.currentThread().getContextClassLoader().getResourceAsStream(CREATE_TABLES_SQL);
+            // See what tables are in the database
+            ResultSet resultSet = statement.executeQuery("SHOW TABLES");
+            if (!resultSet.next()) {
+                // Our tables don't exist so we need to create them
 
-            if (inputStream != null) {
+                // Load the statements and execute each one
+                InputStream inputStream = readFile(CREATE_TABLES_SQL_FILENAME);
 
-                // Split by ; and execute
-                String createTablesScript = IOUtils.toString(inputStream);
-                for (String sqlStatement : createTablesScript.split(";")) {
-                    statement.execute(sqlStatement);
+                if (inputStream != null) {
+                    // Split by ; and execute
+                    String createTablesScript = IOUtils.toString(inputStream);
+                    for (String sqlStatement : createTablesScript.split(";")) {
+                        if (StringUtils.isNotBlank(sqlStatement)) {
+                            statement.execute(sqlStatement);
+                        }
+                    }
+
+                } else {
+                    LOGGER.error("Could not initialise database - could not load {}", CREATE_TABLES_SQL_FILENAME);
+                    throw new RuntimeException("Could not load " + CREATE_TABLES_SQL_FILENAME);
                 }
-
-            } else {
-                LOGGER.error("Could not initialise database - could not load {}", CREATE_TABLES_SQL);
-                throw new RuntimeException("Could not load " + CREATE_TABLES_SQL);
             }
-
         } finally {
             if (statement != null) {
                 statement.close();
@@ -59,7 +74,23 @@ public abstract class BaseDaoTest {
             }
         }
 
+        // Once we've got the tables created populate them with data - clean insert will delete all data first
+        DatabaseDataSourceConnection databaseDataSourceConnection = new DatabaseDataSourceConnection(dataSource);
+
+        // Set the database factory as in http://www.dbunit.org/faq.html#DefaultDataTypeFactory
+        DatabaseConfig config = databaseDataSourceConnection.getConfig();
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());
+        config.setProperty(DatabaseConfig.PROPERTY_ESCAPE_PATTERN, "\"?\"");
+
+        // Construct dataset
+        XmlDataSet dataSet = new XmlDataSet(readFile(DATASET_XML_FILENAME));
+
+        // Insert, cleanly (remove everything first)
+        DatabaseOperation.CLEAN_INSERT.execute(databaseDataSourceConnection, dataSet);
     }
 
+    private InputStream readFile(String filename) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
+    }
 
 }
