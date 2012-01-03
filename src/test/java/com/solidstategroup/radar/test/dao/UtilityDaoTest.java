6575f74e9ccbc3d588193f04195b04e64368b789
David Hemmings <davidh@solidstategroup.com>
2012-01-03 19:23:11 +0000

    Added UtilityDao and tests

---
+++ src/test/java/com/solidstategroup/radar/test/dao/UtilityDaoTest.java
@@ -0,0 +1,40 @@
+package com.solidstategroup.radar.test.dao;
+
+import com.solidstategroup.radar.dao.UtilityDao;
+import com.solidstategroup.radar.model.Centre;
+import com.solidstategroup.radar.model.Country;
+import com.solidstategroup.radar.model.Ethnicity;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+public class UtilityDaoTest extends BaseDaoTest {
+
+    @Autowired
+    private UtilityDao utilityDao;
+
+    @Test
+    public void testGetCentres() {
+        List<Centre> centres = utilityDao.getCentres();
+
+        assertNotNull("Centres list is null", centres);
+    }
+
+    @Test
+    public void testGetCountries() {
+        List<Country> countries = utilityDao.getCountries();
+
+        assertNotNull("Countries list is null", countries);
+    }
+
+    @Test
+    public void testGetEthnicities() {
+        List<Ethnicity> ethnicities = utilityDao.getEthnicities();
+
+        assertNotNull("Ethnicities list is null", ethnicities);
+    }
+
+}
