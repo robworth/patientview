e34bc1f038a641f5481bf6bd386c28cfa26671d5
abul <abul@solidstategroup.com>
2012-01-09 17:30:47 +0000

    done relapse validation

--- src/main/java/com/solidstategroup/radar/web/panels/PathologyPanel.java
+++ src/main/java/com/solidstategroup/radar/web/panels/PathologyPanel.java
@@ -1,7 +1,11 @@
 package com.solidstategroup.radar.web.panels;
 
 import com.solidstategroup.radar.model.sequenced.Pathology;
+import com.solidstategroup.radar.web.RadarApplication;
+import com.solidstategroup.radar.web.components.RadarRequiredDateTextField;
+import com.solidstategroup.radar.web.components.RadarTextFieldWithValidation;
 import com.solidstategroup.radar.web.pages.PatientPage;
+import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.markup.html.form.Form;
@@ -9,6 +13,10 @@ import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.validation.validator.RangeValidator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PathologyPanel extends Panel {
 
@@ -17,27 +25,17 @@ public class PathologyPanel extends Panel {
         setOutputMarkupId(true);
         setOutputMarkupPlaceholderTag(true);
 
+        final List<Component> componentsToUpdate = new ArrayList<Component>();
+
         Form<Pathology> form = new Form<Pathology>("form", new CompoundPropertyModel<Pathology>(new Pathology()));
 
         // Add inputs
+        form.add(new RadarRequiredDateTextField("biopsyDate", RadarApplication.DATE_PATTERN, form, componentsToUpdate));
         form.add(new TextField("sampleLabNumber"));
-        
         form.add(new TextArea("interstitalInflmatoryInfilitrate"));
         form.add(new TextArea("arterialAbnormalities"));
         form.add(new TextArea("immunohistologicalFindings"));
         form.add(new TextArea("electronMicroscopicFindings"));
-
-        form.add(new AjaxSubmitLink("save") {
-            @Override
-            protected void onSubmit(AjaxRequestTarget ajaxRequestTarget, Form<?> form) {
-                // Todo: Implement
-            }
-
-            @Override
-            protected void onError(AjaxRequestTarget ajaxRequestTarget, Form<?> form) {
-                // Todo: Implement
-            }
-        });
 
         form.add(new TextField("estimatedTubules"));
         form.add(new TextField("measuredTubules"));
@@ -49,17 +47,31 @@ public class PathologyPanel extends Panel {
         form.add(new TextField("imageUrl4"));
         form.add(new TextField("imageUrl5"));
 
-        form.add(new TextField("totalNumber"));
-        form.add(new TextField("numberSclerosed"));
-        form.add(new TextField("numberSegmentallySclerosed"));
-        form.add(new TextField("numberCellularCrescents"));
-        form.add(new TextField("numberFibrousCrescents"));
-        form.add(new TextField("numberEndocapillaryHypercelluarity"));
-        form.add(new TextField("numberFibrinoidNecrosis"));
+        form.add(new RadarTextFieldWithValidation("totalNumber", new RangeValidator<Integer>(0, 150), form, componentsToUpdate));
+        form.add(new RadarTextFieldWithValidation("numberSclerosed", new RangeValidator<Integer>(0, 150), form, componentsToUpdate));
+        form.add(new RadarTextFieldWithValidation("numberSegmentallySclerosed", new RangeValidator<Integer>(0, 150), form, componentsToUpdate));
+        form.add(new RadarTextFieldWithValidation("numberCellularCrescents", new RangeValidator<Integer>(0, 150), form, componentsToUpdate));
+        form.add(new RadarTextFieldWithValidation("numberFibrousCrescents", new RangeValidator<Integer>(0, 150), form, componentsToUpdate));
+        form.add(new RadarTextFieldWithValidation("numberEndocapillaryHypercelluarity", new RangeValidator<Integer>(0, 150), form, componentsToUpdate));
+        form.add(new RadarTextFieldWithValidation("numberFibrinoidNecrosis", new RangeValidator<Integer>(0, 150), form, componentsToUpdate));
 
         form.add(new TextArea("otherFeature"));
 
         form.add(new TextArea("histologicalSummary"));
+
+        form.add(new PathologySubmitLink("save", form) {
+            @Override
+            protected List<Component> getComponentsToUpdate() {
+                return componentsToUpdate;
+            }
+        });
+
+        form.add(new PathologySubmitLink("saveDown", form) {
+            @Override
+            protected List<Component> getComponentsToUpdate() {
+                return componentsToUpdate;
+            }
+        });
 
         add(form);
     }
@@ -67,5 +79,24 @@ public class PathologyPanel extends Panel {
     @Override
     public boolean isVisible() {
         return ((PatientPage) getPage()).getCurrentTab().equals(PatientPage.CurrentTab.PATHOLOGY);
+    }
+
+    private abstract class PathologySubmitLink extends AjaxSubmitLink {
+
+        protected PathologySubmitLink(String id, Form<?> form) {
+            super(id, form);
+        }
+
+        @Override
+        protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+            target.add(getComponentsToUpdate().toArray(new Component[getComponentsToUpdate().size()]));
+        }
+
+        @Override
+        protected void onError(AjaxRequestTarget target, Form<?> form) {
+            target.add(getComponentsToUpdate().toArray(new Component[getComponentsToUpdate().size()]));
+        }
+
+        protected abstract List<Component> getComponentsToUpdate();
     }
 }
