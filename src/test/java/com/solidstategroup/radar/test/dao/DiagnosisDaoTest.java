1e69fc44c9cc58f2c1abbef55348fe544678bb16
David Hemmings <davidh@solidstategroup.com>
2012-01-10 12:09:43 +0000

    Implemented getting diagnosis codes

--- src/test/java/com/solidstategroup/radar/test/dao/DiagnosisDaoTest.java
+++ src/test/java/com/solidstategroup/radar/test/dao/DiagnosisDaoTest.java
@@ -2,8 +2,11 @@ package com.solidstategroup.radar.test.dao;
 
 import com.solidstategroup.radar.dao.DiagnosisDao;
 import com.solidstategroup.radar.model.Diagnosis;
+import com.solidstategroup.radar.model.DiagnosisCode;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -18,6 +21,22 @@ public class DiagnosisDaoTest extends BaseDaoTest {
         Diagnosis diagnosis = diagnosisDao.getDiagnosis(117L);
         assertNotNull("Diagnosis is null", diagnosis);
         assertEquals("ID is wrong on diagnosis", new Long(117), diagnosis.getId());
+    }
+
+    @Test
+    public void testGetDiagnosisCode() {
+        DiagnosisCode diagnosisCode = diagnosisDao.getDiagnosisCode(1L);
+        assertNotNull("Diagnosis code was null", diagnosisCode);
+        assertEquals("Wrong ID", new Long(1), diagnosisCode.getId());
+        assertEquals("Wrong desc", "Steroid Resistant Nephrotic Syndrome ", diagnosisCode.getDescription());
+        assertEquals("Wrong abbr", "SRNS", diagnosisCode.getAbbreviation());
+    }
+
+    @Test
+    public void testGetDiagnosisCodes() {
+        List<DiagnosisCode> diagnosisCodes = diagnosisDao.getDiagnosisCodes();
+        assertNotNull("Diagnosis code list was null", diagnosisCodes);
+        assertEquals("Wrong size", 2, diagnosisCodes.size());
     }
 
 }
