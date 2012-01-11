5fa76990b7445c572704dc04494d3dd608dd83a6
David Hemmings <davidh@solidstategroup.com>
2012-01-11 18:18:21 +0000

    Integrated hospitalisation results switcher and add new

--- src/main/java/com/solidstategroup/radar/web/panels/HospitalisationPanel.java
+++ src/main/java/com/solidstategroup/radar/web/panels/HospitalisationPanel.java
@@ -6,14 +6,21 @@ import com.solidstategroup.radar.web.components.RadarDateTextField;
 import com.solidstategroup.radar.web.components.RadarRequiredDateTextField;
 import com.solidstategroup.radar.web.pages.PatientPage;
 import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import java.util.ArrayList;
@@ -30,42 +37,81 @@ public class HospitalisationPanel extends Panel {
         setOutputMarkupId(true);
         setOutputMarkupPlaceholderTag(true);
 
+        // Add container for the form, it is not shown on first visit
+        final MarkupContainer hospitalisationContainer = new WebMarkupContainer("hospitalisationContainer");
+        hospitalisationContainer.setVisible(false);
+        hospitalisationContainer.setOutputMarkupId(true);
+        hospitalisationContainer.setOutputMarkupPlaceholderTag(true);
+        add(hospitalisationContainer);
+
+        // Set up models for the previous results switcher
+        final IModel<Hospitalisation> hospitalisationModel = new Model<Hospitalisation>();
+        final List<Hospitalisation> hospitalisations =
+                hospitalisationDao.getHospitalisationsByRadarNumber(radarNumberModel.getObject());
+
+        // Previous results switcher
+        DropDownChoice<Hospitalisation> switcher =
+                new DropDownChoice<Hospitalisation>("switcher", hospitalisationModel, hospitalisations,
+                        new IChoiceRenderer<Hospitalisation>() {
+                            public Object getDisplayValue(Hospitalisation object) {
+                                // Todo: Figure out what this should show
+                                return object.getId();
+                            }
+
+                            public String getIdValue(Hospitalisation object, int index) {
+                                return String.valueOf(object.getId());
+                            }
+                        });
+        add(switcher);
+
+        // Add ajax behaviour to update form
+        switcher.add(new AjaxFormComponentUpdatingBehavior("onchange") {
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                hospitalisationContainer.setVisible(true);
+                target.add(hospitalisationContainer);
+            }
+        });
+
+        // Add ajax add new
+        add(new AjaxLink("addNew") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                hospitalisationContainer.setVisible(true);
+                target.add(hospitalisationContainer);
+            }
+        });
+
         final List<Component> componentsToUpdate = new ArrayList<Component>();
 
-        CompoundPropertyModel<Hospitalisation> model;
-
         // Set up model
+        CompoundPropertyModel<Hospitalisation> model;
         model = new CompoundPropertyModel<Hospitalisation>(new LoadableDetachableModel<Hospitalisation>() {
             @Override
             protected Hospitalisation load() {
-                if (radarNumberModel.getObject() != null) {
-                    List<Hospitalisation> hospitalisations =
-                            hospitalisationDao.getHospitalisationsByRadarNumber(radarNumberModel.getObject());
-                    if (!hospitalisations.isEmpty()) {
-                        // Todo: This shouldn't just return the first result
-                        return hospitalisations.get(0);
-                    }
+                // If we've got a selected record use that model
+                if (hospitalisationModel.getObject() != null) {
+                    return hospitalisationDao.getHospitalisation(hospitalisationModel.getObject().getId());
                 }
-
+                // Otherwise just return a new object
                 return new Hospitalisation();
             }
         });
 
-        Form<Hospitalisation> form =
-                new Form<Hospitalisation>("form", model) {
-                    @Override
-                    protected void onValidateModelObjects() {
-                        super.onValidateModelObjects();
-                        Hospitalisation hospitalisation = getModelObject();
-                        Date dateofAdmission = hospitalisation.getDateOfAdmission();
-                        Date dateofDischarge = hospitalisation.getDateOfDischarge();
-                        if (dateofAdmission != null && dateofDischarge != null
-                                && dateofAdmission.compareTo(dateofDischarge) != -1) {
-                            get("dateOfDischarge").error("Date has to be after admission date");
-                        }
-                    }
-                };
-        add(form);
+        // Set up the form
+        Form<Hospitalisation> form = new Form<Hospitalisation>("form", model) {
+            @Override
+            protected void onValidateModelObjects() {
+                super.onValidateModelObjects();
+                Hospitalisation hospitalisation = getModelObject();
+                Date dateofAdmission = hospitalisation.getDateOfAdmission();
+                Date dateofDischarge = hospitalisation.getDateOfDischarge();
+                if (dateofAdmission != null && dateofDischarge != null
+                        && dateofAdmission.compareTo(dateofDischarge) != -1) {
+                    get("dateOfDischarge").error("Date has to be after admission date");
+                }
+            }
+        };
 
         form.add(new RadarRequiredDateTextField("dateOfAdmission", form, componentsToUpdate));
         form.add(new RadarDateTextField("dateOfDischarge", form, componentsToUpdate));
@@ -82,6 +128,7 @@ public class HospitalisationPanel extends Panel {
                 ajaxRequestTarget.add(componentsToUpdate.toArray(new Component[componentsToUpdate.size()]));
             }
         });
+        hospitalisationContainer.add(form);
     }
 
     @Override
