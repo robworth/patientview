182f38c42608c8ace702ccca653cd6685d8d362f
David Roberts <david@solidstategroup.com>
2012-02-01 14:53:07 +0000

    Added an authorization strategy so we can work out what login page to send the user to depending on what page they were trying to acccess

--- src/main/java/com/solidstategroup/radar/web/RadarApplication.java
+++ src/main/java/com/solidstategroup/radar/web/RadarApplication.java
@@ -7,11 +7,16 @@ import com.solidstategroup.radar.web.pages.PatientsLoginPage;
 import com.solidstategroup.radar.web.pages.ProfessionalsLoginPage;
 import com.solidstategroup.radar.web.pages.ProfessionalsPage;
 import com.solidstategroup.radar.web.pages.RecruitmentPage;
-import com.solidstategroup.radar.web.pages.RegistrationProfessionalPage;
+import com.solidstategroup.radar.web.pages.ProfessionalRegistrationPage;
 import com.solidstategroup.radar.web.pages.content.ConsentFormsPage;
 import com.solidstategroup.radar.web.pages.content.DiseaseIndexPage;
 import com.solidstategroup.radar.web.pages.content.MpgnPage;
 import com.solidstategroup.radar.web.pages.content.SrnsPage;
+import org.apache.wicket.Component;
+import org.apache.wicket.Page;
+import org.apache.wicket.RestartResponseAtInterceptPageException;
+import org.apache.wicket.authorization.IUnauthorizedComponentInstantiationListener;
+import org.apache.wicket.authorization.UnauthorizedInstantiationException;
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
 import org.apache.wicket.markup.html.WebPage;
@@ -33,8 +38,9 @@ public class RadarApplication extends AuthenticatedWebApplication {
 
     @Override
     protected Class<? extends WebPage> getSignInPageClass() {
-        // Not sure if we can work out which one it needs to be, probably a redirect according to the referring page
-        return ProfessionalsLoginPage.class;
+        // dont actually need to do anything here as there is a unauthorised listener set on the application
+        // which checks what page was requested and decides which login to send to
+        return null;
     }
 
     @Override
@@ -44,12 +50,28 @@ public class RadarApplication extends AuthenticatedWebApplication {
         // This allows our SpringBean annotations to work
         getComponentInstantiationListeners().add(new SpringComponentInjector(this));
 
+        // set a security listener for checks on pages and what logins they should go to
+        // TODO: may have an issue with this as professional and patients both use the PatientPage
+        getSecuritySettings().setUnauthorizedComponentInstantiationListener(
+                new IUnauthorizedComponentInstantiationListener() {
+                    public void onUnauthorizedInstantiation(final Component component) {
+                        if (component instanceof Page) {
+                            if (component.getClass() == PatientPage.class) {
+                                throw new RestartResponseAtInterceptPageException(PatientsLoginPage.class);
+                            }
+
+                            throw new RestartResponseAtInterceptPageException(ProfessionalsLoginPage.class);
+                        } else {
+                            throw new UnauthorizedInstantiationException(component.getClass());
+                        }
+                    }
+        });
         // Mount nice URLs for pages - patient pages
         mountPage("patient", PatientPage.class);
         mountPage("patients", ExistingPatientsPage.class);
         mountPage("login", PatientsLoginPage.class);
 
-        mountPage("registration", RegistrationProfessionalPage.class);
+        mountPage("registration", ProfessionalRegistrationPage.class);
         mountPage("professionals", ProfessionalsPage.class);
         mountPage("recruitment", RecruitmentPage.class);
 
