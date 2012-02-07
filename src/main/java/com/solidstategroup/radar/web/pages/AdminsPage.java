76a4278b0b07025148a85245a670ceb6f2c8fc89
David Roberts <david@solidstategroup.com>
2012-02-07 13:54:40 +0000

    new ladning page for logged in admin users

---
+++ src/main/java/com/solidstategroup/radar/web/pages/AdminsPage.java
@@ -0,0 +1,5 @@
+package com.solidstategroup.radar.web.pages;
+
+// nothing actually on this page its just what the user lands on after being logged in
+public class AdminsPage extends AdminsBasePage {
+}
