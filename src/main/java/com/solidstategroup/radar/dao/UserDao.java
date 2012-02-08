15f0c542eae0537a48a0c26055d00f215df18a6c
David Roberts <david@solidstategroup.com>
2012-02-08 13:51:26 +0000

    added in methods to search and retrieve Profesional Users

--- src/main/java/com/solidstategroup/radar/dao/UserDao.java
+++ src/main/java/com/solidstategroup/radar/dao/UserDao.java
@@ -1,8 +1,11 @@
 package com.solidstategroup.radar.dao;
 
+import com.solidstategroup.radar.model.filter.ProfessionalUserFilter;
 import com.solidstategroup.radar.model.user.AdminUser;
 import com.solidstategroup.radar.model.user.PatientUser;
 import com.solidstategroup.radar.model.user.ProfessionalUser;
+
+import java.util.List;
 
 public interface UserDao {
 
@@ -13,4 +16,10 @@ public interface UserDao {
     void savePatientUser(PatientUser patientUser);
 
     ProfessionalUser getProfessionalUser(String email);
+    
+    List<ProfessionalUser> getProfessionalUsers();
+    
+    List<ProfessionalUser> getProfessionalUsers(ProfessionalUserFilter filter);
+    
+    List<ProfessionalUser> getProfessionalUsers(ProfessionalUserFilter filter, int page, int numberPerPage);
 }
