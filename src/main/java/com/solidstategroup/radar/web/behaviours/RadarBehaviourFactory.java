01529f6c33e468de3a94a16a00e3c0a3e9a2b83c
David Roberts <david@solidstategroup.com>
2012-02-13 14:06:34 +0000

    checkstyle

--- src/main/java/com/solidstategroup/radar/web/behaviours/RadarBehaviourFactory.java
+++ src/main/java/com/solidstategroup/radar/web/behaviours/RadarBehaviourFactory.java
@@ -1,14 +1,12 @@
 package com.solidstategroup.radar.web.behaviours;
 
-
 import org.apache.wicket.model.Model;
-
 import java.io.Serializable;
 
 public class RadarBehaviourFactory {
 
-    public static AttributePrepender getDeleteConfirmationBehaviour(){
+    public static AttributePrepender getDeleteConfirmationBehaviour() {
         return AttributePrepender.getNewInstance("onclick", new Model<Serializable>("if(!confirm('Are you sure " +
-                    "you want to delete this record?')){return false;}"));
+                "you want to delete this record?')){return false;}"));
     }
 }
