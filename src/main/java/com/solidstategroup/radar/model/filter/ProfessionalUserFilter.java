1d454e49355f737acbc43c87ad49634bed9c2669
David Roberts <david@solidstategroup.com>
2012-02-14 12:34:56 +0000

    extending

--- src/main/java/com/solidstategroup/radar/model/filter/ProfessionalUserFilter.java
+++ src/main/java/com/solidstategroup/radar/model/filter/ProfessionalUserFilter.java
@@ -1,11 +1,6 @@
 package com.solidstategroup.radar.model.filter;
 
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-
-public class ProfessionalUserFilter  implements Serializable {
-
+public class ProfessionalUserFilter extends BaseFilter {
     public enum UserField {
         SURNAME("uSurname"),
         FORENAME("uForename"),
@@ -22,65 +17,23 @@ public class ProfessionalUserFilter  implements Serializable {
             this.databaseFieldName = databaseFieldName;
         }
 
+        public static UserField getUserField(String fieldName) {
+            for (UserField userField : values()) {
+                if (userField.getDatabaseFieldName().equalsIgnoreCase(fieldName)) {
+                    return userField;
+                }
+            }
+
+            return UserField.SURNAME;
+        }
+
         public String getDatabaseFieldName() {
             return databaseFieldName;
         }
     }
-    
-    private Map<UserField, String> searchFields;
-    private UserField sortField;
-    private boolean reverse;
 
     public ProfessionalUserFilter() {
-        searchFields = new HashMap<UserField, String>();
-        sortField = UserField.SURNAME;
-        reverse = true;
+        super();
+        sortField = UserField.SURNAME.getDatabaseFieldName();
     }
-
-    public ProfessionalUserFilter(Map<UserField, String> searchFields, UserField sortField, boolean reverse) {
-        this.searchFields = searchFields;
-        this.sortField = sortField;
-        this.reverse = reverse;
-    }
-
-    public void addSearchCriteria(UserField userField, String searchTerm) {
-        searchFields.put(userField, searchTerm);
-    }
-    
-    public void removeSearchCriteria(UserField userField) {
-        searchFields.remove(userField);
-    }
-
-    public boolean hasSearchFilter() {
-        return searchFields != null && !searchFields.isEmpty();
-    }
-
-    public boolean hasSortFilter() {
-        return sortField != null;
-    }
-
-    public Map<UserField, String> getSearchFields() {
-        return searchFields;
-    }
-
-    public void setSearchFields(Map<UserField, String> searchFields) {
-        this.searchFields = searchFields;
-    }
-
-    public UserField getSortField() {
-        return sortField;
-    }
-
-    public void setSortField(UserField sortField) {
-        this.sortField = sortField;
-    }
-
-    public boolean isReverse() {
-        return reverse;
-    }
-
-    public void setReverse(boolean reverse) {
-        this.reverse = reverse;
-    }
-
 }
