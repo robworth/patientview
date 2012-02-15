5b8519f2e4309e30fd87b94d4d7526450b3a7f5b
David Roberts <david@solidstategroup.com>
2012-02-15 15:38:58 +0000

    added helper methods for building sql queries

--- src/main/java/com/solidstategroup/radar/dao/impl/BaseDaoImpl.java
+++ src/main/java/com/solidstategroup/radar/dao/impl/BaseDaoImpl.java
@@ -4,11 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.apache.commons.lang.StringUtils;
 
 import javax.sql.DataSource;
 import java.lang.reflect.Method;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public abstract class BaseDaoImpl {
 
@@ -87,5 +92,139 @@ public abstract class BaseDaoImpl {
         return value;
     }
 
+    /**
+     * Build an update query for a database record
+     * @param tableName table to update
+     * @param idFieldName the field in the table which is used as the identifier for the record to update
+     * @param paramMap List of values to update on the table and the field they should update
+     * @return String
+     */
+    public String buildUpdateQuery(String tableName, String idFieldName, Map<String, Object> paramMap) {
+        String updateSql = "UPDATE " + tableName + " SET ";
 
+        // get the id value from the map
+        Object idValue = paramMap.get(idFieldName);
+
+        // remove the id temp so we can loop through it as we dont want that in the update part
+        paramMap.remove(idFieldName);
+
+        // loop through all params and add them in
+        int count = 1;
+        for (String field : paramMap.keySet()) {
+            updateSql += " " + field + " = :" + field;
+
+            if (count < paramMap.size()) {
+                updateSql += ", ";
+            }
+
+            count++;
+        }
+
+        // add the where clause with the id
+        updateSql += " WHERE " + idFieldName + " = :" + idFieldName;
+
+        // add it back into map
+        paramMap.put(idFieldName, idValue);
+
+        return updateSql;
+    }
+
+    /**
+     * Will build the order by statement on the specified field and what directon its to be returned in
+     * @param sortField Database field name
+     * @param reverse whether its ASC or DESC
+     * @return String
+     */
+    public String buildOrderQuery(String sortField, boolean reverse) {
+        return "ORDER BY " + sortField + " " + (reverse ? "ASC" : "DESC");       
+    }
+
+    /**
+     * Will simple take two values to start the record set from and end it
+     * @param page Start record
+     * @param numberPerPage Number of records to return
+     * @param paramList List of params for current query
+     * @return String
+     */
+    public String buildLimitQuery(int page, int numberPerPage, List<Object> paramList) {
+        if (page > 0 && numberPerPage > 0) {
+            // work out the row to start from
+            int start = ((page * numberPerPage) - numberPerPage);
+
+            paramList.add(start);
+            paramList.add(numberPerPage);
+
+            return "LIMIT ?, ?";
+        }
+
+        return "";
+    }
+
+    /**
+     * Will build a where query based on search values in a map
+     * Key in the map is the database field name and the value is the text to search for
+     * @param searchMap Map<String, String> fieldName, searchValue
+     * @param and If the where clause should AND or OR the search values
+     * @param paramList List of params for current query
+     * @return String
+     */
+    public String buildWhereQuery(Map<String, String> searchMap, boolean and, List<Object> paramList) {
+        if (searchMap != null && !searchMap.isEmpty()) {
+            List<String> searchQueries = new ArrayList<String>();
+
+            // if there a search fields in the filter then create where clause
+            searchQueries.add("WHERE");
+
+            // parse the search map as one key can be multiple fields in a table
+            searchMap = parseSearchMap(searchMap);
+            
+            int count = 1;
+            for (Map.Entry<String, String> entry : searchMap.entrySet()) {
+                if (entry.getValue().length() > 0) {
+                    // converting the field values to uppercase so I dont have to faff around
+                    // probably bite me in the ass at some point
+                    searchQueries.add("UPPER(" + entry.getKey() + ") LIKE ?");
+                    paramList.add("%" + entry.getValue().toUpperCase() + "%");
+
+                    // if there are more than one field being search AND them
+                    if (count < paramList.size()) {
+                        searchQueries.add((and ? "AND" : "OR"));
+                    }
+
+                    count++;
+                }
+            }
+
+            return StringUtils.join(searchQueries.toArray(), " ");
+        }
+
+        return "";
+    }
+
+    /**
+     * Will take a Map<String, String> and check to see if the key is a multi part value seperated by a , indicating
+     * that the search value should be used across multiple database fields
+     * If it is it will split and add in a key for each all using the same search value
+     * @param searchMap Map<String, String>
+     * @return Map<String, String>
+     */
+    private Map<String, String> parseSearchMap(Map<String, String> searchMap) {
+        Map<String, String> newSearchMap = new HashMap<String, String>();
+
+        for (Map.Entry<String, String> entry : searchMap.entrySet()) {
+            String searchValue = entry.getValue().trim();
+
+            if (entry.getKey().indexOf(",") > -1) {
+                String[] fields = entry.getKey().split(",");
+
+                for (String s : fields) {
+                    newSearchMap.put(s.trim(), searchValue);
+                }
+            } else {
+                newSearchMap.put(entry.getKey().trim(), searchValue);
+            }
+        }
+
+        return newSearchMap;
+    }
 }
