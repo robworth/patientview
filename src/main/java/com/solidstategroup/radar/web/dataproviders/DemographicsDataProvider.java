9870722b71eeaf8821d264ac321481ba3e0a65bf
David Roberts <david@solidstategroup.com>
2012-02-16 09:23:33 +0000

    using new filter

--- src/main/java/com/solidstategroup/radar/web/dataproviders/DemographicsDataProvider.java
+++ src/main/java/com/solidstategroup/radar/web/dataproviders/DemographicsDataProvider.java
@@ -1,9 +1,10 @@
 package com.solidstategroup.radar.web.dataproviders;
 
-import com.solidstategroup.radar.dao.DemographicsDao;
 import com.solidstategroup.radar.model.Centre;
 import com.solidstategroup.radar.model.Demographics;
+import com.solidstategroup.radar.model.filter.DemographicsFilter;
 import com.solidstategroup.radar.service.DemographicsManager;
+import com.solidstategroup.radar.web.dataproviders.user.SortableProvider;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
@@ -11,35 +12,69 @@ import org.apache.wicket.model.IModel;
 import java.util.Iterator;
 import java.util.List;
 
-public class DemographicsDataProvider implements IDataProvider<Demographics> {
+public class DemographicsDataProvider implements IDataProvider<Demographics>, SortableProvider {
 
     private Centre centre;
     private DemographicsManager demographicsManager;
+    private DemographicsFilter demographicsFilter;
 
     public DemographicsDataProvider(DemographicsManager demographicsManager, Centre centre) {
-        this.demographicsManager = demographicsManager;
+        this(demographicsManager);
+
         this.centre = centre;
     }
 
     public DemographicsDataProvider(DemographicsManager demographicsManager) {
         this.demographicsManager = demographicsManager;
+        demographicsFilter = new DemographicsFilter();
     }
 
-    public Iterator<? extends Demographics> iterator(int first, int count) {
-        // Todo: Paging
-        return getResults().iterator();
+    public Iterator<? extends Demographics> iterator(int i, int i1) {
+        int pageNumber = (i / i1) + 1;
+        return getResults(pageNumber, i1).iterator();
     }
 
     public int size() {
-        return getResults().size();
+        return getResults(-1, -1).size();
     }
 
-    private List<Demographics> getResults() {
+    public void setAscending(boolean ascending) {
+        demographicsFilter.setReverse(ascending);
+    }
 
+    public boolean isAscending() {
+        return demographicsFilter.isReverse();
+    }
+
+    public void setSortField(String sortField) {
+        demographicsFilter.setSortField(sortField);
+    }
+
+    public String getSortField() {
+        return demographicsFilter.getSortField();
+    }
+
+    public boolean hasSearchCriteria() {
+        return demographicsFilter.hasSearchCriteria();
+    }
+
+    public void addSearchCriteria(String searchField, String searchText) {
+        demographicsFilter.addSearchCriteria(searchField, searchText);
+    }
+
+    public void removeSearchCriteria(String searchField) {
+        demographicsFilter.removeSearchCriteria(searchField);
+    }
+
+    public void clearSearchCriteria() {
+        demographicsFilter.getSearchFields().clear();
+    }
+
+    private List<Demographics> getResults(int page, int resultsPerPage) {
         if (centre != null) {
             return demographicsManager.getDemographicsByRenalUnit(centre);
         } else {
-            return demographicsManager.getDemographics();
+            return demographicsManager.getDemographics(demographicsFilter, page, resultsPerPage);
         }
     }
 
