02388ba833dfd162dac9eab1943b9b098fa3e544
David Roberts <david@solidstategroup.com>
2012-02-17 09:22:49 +0000

    added custom navigation

--- src/main/java/com/solidstategroup/radar/web/pages/admin/AdminsBasePage.java
+++ src/main/java/com/solidstategroup/radar/web/pages/admin/AdminsBasePage.java
@@ -2,8 +2,17 @@ package com.solidstategroup.radar.web.pages.admin;
 
 import com.solidstategroup.radar.model.user.User;
 import com.solidstategroup.radar.web.pages.BasePage;
+import com.solidstategroup.radar.web.panels.navigation.AdminNavigationPanel;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.Page;
 
 @AuthorizeInstantiation({User.ROLE_ADMIN})
 public class AdminsBasePage extends BasePage {
+    public AdminsBasePage() {
+        super();
+    }
+
+    protected void addNavigation(Class<? extends Page> pageClass) {
+        add(new AdminNavigationPanel());
+    }
 }
