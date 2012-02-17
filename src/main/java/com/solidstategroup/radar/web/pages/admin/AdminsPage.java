6ddb80dd0c47675b885b060fd25189166d4d7606
David Roberts <david@solidstategroup.com>
2012-02-17 09:22:03 +0000

    moved the links into the navigation

--- src/main/java/com/solidstategroup/radar/web/pages/admin/AdminsPage.java
+++ src/main/java/com/solidstategroup/radar/web/pages/admin/AdminsPage.java
@@ -1,13 +1,7 @@
 package com.solidstategroup.radar.web.pages.admin;
 
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-
 // nothing actually on this page its just what the user lands on after being logged in
 public class AdminsPage extends AdminsBasePage {
     public AdminsPage() {
-        add(new BookmarkablePageLink<AdminUsersPage>("adminUsersPageLink", AdminUsersPage.class));
-        add(new BookmarkablePageLink<AdminConsultantsPage>("adminConsultantsPageLink", AdminConsultantsPage.class));
-        add(new BookmarkablePageLink<AdminPatientsAllPage>("adminPatientsAllPageLink", AdminPatientsAllPage.class));
-        add(new BookmarkablePageLink<AdminPatientsPage>("adminPatientsPageLink", AdminPatientsPage.class));
     }
 }
