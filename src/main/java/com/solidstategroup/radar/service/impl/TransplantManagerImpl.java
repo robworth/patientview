37a5be89db26fff27005b698c6cd9f5a63fae1d8
abul <abul@solidstategroup.com>
2012-02-27 14:24:33 +0000

    RAD-52 - transplant validation

--- src/main/java/com/solidstategroup/radar/service/impl/TransplantManagerImpl.java
+++ src/main/java/com/solidstategroup/radar/service/impl/TransplantManagerImpl.java
@@ -1,18 +1,90 @@
 package com.solidstategroup.radar.service.impl;
 
 import com.solidstategroup.radar.dao.TransplantDao;
+import com.solidstategroup.radar.dao.impl.TransplantDaoImpl;
 import com.solidstategroup.radar.model.Transplant;
+import com.solidstategroup.radar.model.exception.InvalidModelException;
 import com.solidstategroup.radar.service.TransplantManager;
-import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.jfree.data.time.Day;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.Years;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 
 public class TransplantManagerImpl implements TransplantManager {
 
     TransplantDao transplantDao;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransplantDaoImpl.class);
 
-    public void saveTransplant(Transplant transplant) {
+    public void saveTransplant(Transplant transplant) throws InvalidModelException {
+        // validate transplant
+        List<String> errors = new ArrayList<String>();
+        List<Transplant> transplants = transplantDao.getTransplantsByRadarNumber(transplant.getRadarNumber());
+
+        // date of recurrance must be greater than transplant date
+        if (transplant.getDateRecurr() != null) {
+            if (transplant.getDate().compareTo(transplant.getDateRecurr()) >= 0) {
+                errors.add("Recurrance date must be greate than transplant date");
+            }
+        }
+
+        // transplant must be 14 days apart
+        for (Transplant existingTransplant : transplants) {
+            if (existingTransplant.getId().equals(transplant.getId())) {
+                continue;
+            }
+            int daysApart = Math.abs(Days.daysBetween(new DateTime(existingTransplant.getDate()),
+                    new DateTime(transplant.getDate())).getDays());
+            if (Math.abs(daysApart) <= 14) {
+                errors.add(TRANSPLANTS_INTERVAL_ERROR);
+            }
+        }
+
+        // cannot add new transplant whilst previous transplant has not failed
+        for (Transplant existingTransplant : transplants) {
+            if (existingTransplant.getId().equals(transplant.getId())) {
+                continue;
+            }
+            if (existingTransplant.getDateFailureRejectData() != null) {
+                if (existingTransplant.getDateFailureRejectData().getFailureDate() == null) {
+                    errors.add(PREVIOUS_TRANSPLANT_NOT_FAILED_ERROR);
+                    break;
+                }
+            } else {
+                errors.add(PREVIOUS_TRANSPLANT_NOT_FAILED_ERROR);
+                break;
+            }
+        }
+
+        // cannot add transplant before another failure date
+        for (Transplant existingTransplant : transplants) {
+            if (existingTransplant.getId().equals(transplant.getId())) {
+                continue;
+            }
+            if (existingTransplant.getDateFailureRejectData() != null) {
+                if (existingTransplant.getDateFailureRejectData().getFailureDate() != null) {
+                    Date failureDate = existingTransplant.getDateFailureRejectData().getFailureDate();
+                    if (failureDate.compareTo(transplant.getDate()) > 0) {
+                        errors.add(BEFORE_PREVIOUS_FAILURE_DATE);
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        if (!errors.isEmpty()) {
+            InvalidModelException exception = new InvalidModelException("Transplant model is not valid");
+            exception.setErrors(errors);
+            throw exception;
+        }
         transplantDao.saveTransplant(transplant);
     }
 
