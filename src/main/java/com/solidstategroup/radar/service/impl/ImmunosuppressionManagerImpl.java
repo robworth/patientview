170f69c5d572dc92b02951caa8b26f9570fafd4a
abul <abul@solidstategroup.com>
2012-02-27 16:57:52 +0000

    RAD-50 - immunosupression treatment validation

--- src/main/java/com/solidstategroup/radar/service/impl/ImmunosuppressionManagerImpl.java
+++ src/main/java/com/solidstategroup/radar/service/impl/ImmunosuppressionManagerImpl.java
@@ -3,9 +3,10 @@ package com.solidstategroup.radar.service.impl;
 import com.solidstategroup.radar.dao.ImmunosuppressionDao;
 import com.solidstategroup.radar.model.Immunosuppression;
 import com.solidstategroup.radar.model.ImmunosuppressionTreatment;
+import com.solidstategroup.radar.model.exception.InvalidModelException;
 import com.solidstategroup.radar.service.ImmunosuppressionManager;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -13,12 +14,57 @@ public class ImmunosuppressionManagerImpl implements ImmunosuppressionManager {
 
     ImmunosuppressionDao immunosuppressionDao;
 
-    public void saveImmunosuppressionTreatment(ImmunosuppressionTreatment immunosuppressionTreatment) {
-        immunosuppressionDao.saveImmunosuppressionTreatment(immunosuppressionTreatment);
+    public void saveImmunosuppressionTreatment(ImmunosuppressionTreatment immunosuppression) throws
+            InvalidModelException {
+
+
+        // validation
+        List<String> errors = new ArrayList<String>();
+        List<ImmunosuppressionTreatment> ummunosupressionList = immunosuppressionDao.
+                getImmunosuppressionTreatmentByRadarNumber(immunosuppression.getRadarNumber());
+
+        //  must have finish date before you can start it again
+        for (ImmunosuppressionTreatment existingImmunosuppression : ummunosupressionList) {
+            if (existingImmunosuppression.getId().equals(immunosuppression.getId())) {
+                continue;
+            }
+            if (existingImmunosuppression.getEndDate() == null) {
+                errors.add(PREVIOUS_TREATMENT_NOT_STOPPED_ERROR);
+                break;
+            }
+        }
+
+        // dates must not overlap
+        for (ImmunosuppressionTreatment existingImmunosuppression : ummunosupressionList) {
+            if (existingImmunosuppression.getId().equals(immunosuppression.getId())) {
+                continue;
+            }
+            if (existingImmunosuppression.getEndDate() != null) {
+                if (immunosuppression.getStartDate().compareTo(existingImmunosuppression.getStartDate()) >= 0 &&
+                        immunosuppression.getStartDate().compareTo(existingImmunosuppression.getEndDate()) < 1) {
+                    errors.add(OVERLAPPING_ERROR);
+                    break;
+                }
+                if (immunosuppression.getEndDate() != null) {
+                    if (immunosuppression.getEndDate().compareTo(existingImmunosuppression.getStartDate()) >= 0 &&
+                            immunosuppression.getEndDate().compareTo(existingImmunosuppression.getEndDate()) < 1) {
+                        errors.add(OVERLAPPING_ERROR);
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (!errors.isEmpty()) {
+            InvalidModelException exception = new InvalidModelException("immunosupression model is not valid");
+            exception.setErrors(errors);
+            throw exception;
+        }
+        immunosuppressionDao.saveImmunosuppressionTreatment(immunosuppression);
     }
 
     public void deleteImmunosuppressionTreatment(ImmunosuppressionTreatment immunosuppressionTreatment) {
-         immunosuppressionDao.deleteImmunosuppressionTreatment(immunosuppressionTreatment);
+        immunosuppressionDao.deleteImmunosuppressionTreatment(immunosuppressionTreatment);
     }
 
     public ImmunosuppressionTreatment getImmunosuppressionTreatment(long id) {
