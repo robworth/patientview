267a39c462c45a6f4016d43534824d8237172814
abul <abul@solidstategroup.com>
2012-03-07 18:42:53 +0000

    RAD-76 - glomeril total value

--- src/main/java/com/solidstategroup/radar/service/impl/PathologyManagerImpl.java
+++ src/main/java/com/solidstategroup/radar/service/impl/PathologyManagerImpl.java
@@ -1,17 +1,42 @@
 package com.solidstategroup.radar.service.impl;
 
 import com.solidstategroup.radar.dao.PathologyDao;
+import com.solidstategroup.radar.model.exception.InvalidModelException;
 import com.solidstategroup.radar.model.sequenced.Pathology;
 import com.solidstategroup.radar.service.PathologyManager;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PathologyManagerImpl implements PathologyManager {
 
     private PathologyDao pathologyDao;
 
-    public void savePathology(Pathology pathology) {
+    public void savePathology(Pathology pathology) throws InvalidModelException {
+        // validate
+        List<String> errors = new ArrayList<String>();
+
+        int total = pathology.getTotalNumber() != null ? pathology.getTotalNumber() : 0;
+        int sum = 0;
+        for (Integer value : Arrays.asList(pathology.getNumberSclerosed(), pathology.getNumberSegmentallySclerosed(),
+                pathology.getNumberCellularCrescents(), pathology.getNumberFibrousCrescents(),
+                pathology.getNumberEndocapillaryHypercelluarity(), pathology.getNumberFibrinoidNecrosis())) {
+
+            sum += value != null ? value : 0;
+        }
+
+        if (sum > total) {
+            errors.add(TOTAL_ERROR);
+        }
+
+        if (!errors.isEmpty()) {
+            InvalidModelException exception = new InvalidModelException("Pathology model is not valid");
+            exception.setErrors(errors);
+            throw exception;
+        }
+
         pathologyDao.savePathology(pathology);
     }
 
