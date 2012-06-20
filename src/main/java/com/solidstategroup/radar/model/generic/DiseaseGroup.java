221c9f668217421e0f00b82196c2539e340d98cd
abul <abul@solidstategroup.com>
2012-06-20 12:03:38 +0100

    integrate data import

--- src/main/java/com/solidstategroup/radar/model/generic/DiseaseGroup.java
+++ src/main/java/com/solidstategroup/radar/model/generic/DiseaseGroup.java
@@ -9,8 +9,8 @@ import java.io.Serializable;
  * rdr_disease_group
  */
 public class DiseaseGroup extends BaseModel implements Serializable{
-    public static final Long SRNS_DISEASE_GROUP_ID = 6L; // todo change this for live and check ids for dev
-    public static final Long MPGN_DISEASEGROUP_ID = 7L; // todo copy this for live and check ids for dev
+    public static final Long SRNS_DISEASE_GROUP_ID = 42L; // todo change this for live and check ids for dev
+    public static final Long MPGN_DISEASEGROUP_ID = 32L; // todo copy this for live and check ids for dev
     private String name;
 
     public String getName() {
