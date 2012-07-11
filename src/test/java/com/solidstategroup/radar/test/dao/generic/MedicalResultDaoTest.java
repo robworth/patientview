e8919089c72565e0cc2bb370c12f4c2928dd53cd
David Roberts <david@solidstategroup.com>
2012-07-11 10:43:35 +0100

    updated to work with new code

--- src/test/java/com/solidstategroup/radar/test/dao/generic/MedicalResultDaoTest.java
+++ src/test/java/com/solidstategroup/radar/test/dao/generic/MedicalResultDaoTest.java
@@ -1,6 +1,5 @@
 package com.solidstategroup.radar.test.dao.generic;
 
-
 import com.solidstategroup.radar.dao.generic.DiseaseGroupDao;
 import com.solidstategroup.radar.dao.generic.MedicalResultDao;
 import com.solidstategroup.radar.model.generic.DiseaseGroup;
@@ -20,48 +19,41 @@ public class MedicalResultDaoTest extends BaseDaoTest {
     @Autowired
     DiseaseGroupDao diseaseGroupDao;
 
-//    @Test
-//    public void testSave() throws Exception {
-        // save new record
-//        Date date = new Date();
-//
-//        MedicalResult medicalResult = new MedicalResult();
-//        medicalResult.setRadarNo(241L);
-//        medicalResult.setSerumCreatanine(10.25);
-//        medicalResult.setAntihypertensiveDrugs(MedicalResult.YesNo.YES);
-//        medicalResult.setBloodUrea(12.25);
-//        medicalResult.setBloodUreaDate(date);
-//        medicalResult.setSerumCreatanine(15.5);
-//        medicalResult.setCreatanineDate(date);
-//        medicalResult.setBpDiastolic(18);
-//        medicalResult.setBpSystolic(19);
-//        medicalResult.setHeight(100.5);
-//        medicalResult.setHeightDate(date);
-//        medicalResult.setWeight(122.0);
-//        medicalResult.setWeightDate(date);
-//        medicalResult.setBpDate(date);
-//
-//        DiseaseGroup diseaseGroup = diseaseGroupDao.getById("1");
-//        medicalResult.setDiseaseGroup(diseaseGroup);
-//
-//        medicalResultDao.save(medicalResult);
-//
-//        medicalResult = medicalResultDao.getMedicalResult(241L, diseaseGroup.getId());
-//
-//        Assert.assertNotNull("Medical result should not be null", medicalResult);
-//
-//        // update record
-//        medicalResult.setBloodUrea(15.5);
-//        medicalResultDao.save(medicalResult);
-//
-//        medicalResult = medicalResultDao.getMedicalResult(241L, diseaseGroup.getId());
-//        Assert.assertEquals("Blood urea has wrong value", new Double(15.5), medicalResult.getBloodUrea());
-//    }
-//
-//    @Test
-//    public void testGetById() throws Exception {
-        //MedicalResult medicalResult = medicalResultDao.getById("1");
-        //Assert.assertNotNull(medicalResult);
-//    }
+    @Test
+    public void testSave() throws Exception {
+        //save new record
+        Date date = new Date();
 
+        MedicalResult medicalResult = new MedicalResult();
+        medicalResult.setRadarNo(1L);
+        medicalResult.setSerumCreatanine(10.25);
+        medicalResult.setAntihypertensiveDrugs(MedicalResult.YesNo.YES);
+        medicalResult.setBloodUrea(12.25);
+        medicalResult.setBloodUreaDate(date);
+        medicalResult.setSerumCreatanine(15.5);
+        medicalResult.setCreatanineDate(date);
+        medicalResult.setBpDiastolic(18);
+        medicalResult.setBpSystolic(19);
+        medicalResult.setHeight(100.5);
+        medicalResult.setHeightDate(date);
+        medicalResult.setWeight(122.0);
+        medicalResult.setWeightDate(date);
+        medicalResult.setBpDate(date);
+
+        DiseaseGroup diseaseGroup = diseaseGroupDao.getById("1");
+        medicalResult.setDiseaseGroup(diseaseGroup);
+
+        medicalResultDao.save(medicalResult);
+
+        medicalResult = medicalResultDao.getMedicalResult(1L, diseaseGroup.getId());
+
+        Assert.assertNotNull("Medical result should not be null", medicalResult);
+
+        // update record
+        medicalResult.setBloodUrea(15.5);
+        medicalResultDao.save(medicalResult);
+
+        medicalResult = medicalResultDao.getMedicalResult(1L, diseaseGroup.getId());
+        Assert.assertEquals("Blood urea has wrong value", new Double(15.5), medicalResult.getBloodUrea());
+    }
 }
