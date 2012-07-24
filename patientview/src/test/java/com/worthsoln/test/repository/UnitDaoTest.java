d38e5a8ef307bec25dd59be385d92af87facfed6
Paul Chenery <paul@solidstategroup.com>
2012-07-24 15:32:35 +0100

    unit dao tests and implementations

--- patientview/src/test/java/com/worthsoln/test/repository/UnitDaoTest.java
+++ patientview/src/test/java/com/worthsoln/test/repository/UnitDaoTest.java
@@ -8,9 +8,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -64,7 +66,7 @@ public class UnitDaoTest extends BaseDaoTest {
         Unit unit = new Unit();
         // required fields
         unit.setUnitcode("UNITCODE1");
-        unit.setName("name1");
+        unit.setName("z");
         unit.setShortname("nam1");
         // not required
         unit.setUnituser("user1");
@@ -74,7 +76,7 @@ public class UnitDaoTest extends BaseDaoTest {
         unit = new Unit();
         // required fields
         unit.setUnitcode("UNITCODE2");
-        unit.setName("name2");
+        unit.setName("y");
         unit.setShortname("nam2");
         // not required
         unit.setUnituser("user1");
@@ -84,7 +86,7 @@ public class UnitDaoTest extends BaseDaoTest {
         unit = new Unit();
         // required fields
         unit.setUnitcode("UNITCODE3");
-        unit.setName("name3");
+        unit.setName("x");
         unit.setShortname("nam3");
         // not required
         unit.setUnituser("user2");
@@ -94,10 +96,10 @@ public class UnitDaoTest extends BaseDaoTest {
         unit = new Unit();
         // required fields
         unit.setUnitcode("UNITCODE4");
-        unit.setName("name4");
+        unit.setName("w");
         unit.setShortname("nam4");
         // not required
-        unit.setUnituser("user2");
+        unit.setUnituser("");  // no user
 
         unitDao.save(unit);
     }
@@ -125,5 +127,98 @@ public class UnitDaoTest extends BaseDaoTest {
         checkUnit = unitDao.get(unit.getUnitcode());
 
         assertTrue("Unit not found with unitcode", checkUnit != null && checkUnit.getId() > 0);
+    }
+
+    @Test
+    public void testGetAllSort() {
+
+        List<Unit> units = unitDao.getAll(false);
+
+        assertEquals("Incorrect number of units found", 4, units.size());
+        assertEquals("Incorrect first unit", "UNITCODE1", units.get(0).getUnitcode());
+
+        units = unitDao.getAll(true);
+
+        assertEquals("Incorrect number of units found", 4, units.size());
+        assertEquals("Incorrect first unit", "UNITCODE4", units.get(0).getUnitcode());
+    }
+
+    @Test
+    public void testGetUnitsWIthUser() {
+
+        assertEquals("Incorrect number of units with user", 3, unitDao.getUnitsWithUser().size());
+    }
+
+    @Test
+    public void testWithUnitCodes() {
+        List<String> unitCodes = new ArrayList<String>();
+        unitCodes.add("UNITCODE2");
+        unitCodes.add("UNITCODE4");
+
+        List<Unit> units = unitDao.get(unitCodes);
+
+        assertEquals("Incorrect number of units", 2, units.size());
+    }
+
+    @Test
+    public void testNotTheseUnitCodes() {
+
+        List<String> unitCodes = new ArrayList<String>();
+        String[] notTheseUnitCodes = new String[] {"UNITCODE2", "UNITCODE4"};
+
+        List<Unit> units = unitDao.get(unitCodes, notTheseUnitCodes, new String[]{});
+
+        assertEquals("Incorrect number of units", 2, units.size());
+        assertEquals("Incorrect first unit", "UNITCODE3", units.get(0).getUnitcode());
+        assertEquals("Incorrect last unit", "UNITCODE1", units.get(1).getUnitcode());
+    }
+
+    @Test
+    public void testNotTheseUnitCodesPlusUnitCode() {
+
+        Unit unit = new Unit();
+        // required fields
+        unit.setUnitcode("UNITCODE5");
+        unit.setName("za");
+        unit.setShortname("nam5");
+        // not required
+        unit.setUnituser("user5");
+
+        unitDao.save(unit);
+
+        List<String> unitCodes = new ArrayList<String>();
+        String[] notTheseUnitCodes = new String[] {"UNITCODE2", "UNITCODE4"};
+        String[] plusUnitCodes = new String[] {"UNITCODE5"};
+
+        List<Unit> units = unitDao.get(unitCodes, notTheseUnitCodes, plusUnitCodes);
+
+        assertEquals("Incorrect number of units", 3, units.size());
+        assertEquals("Incorrect first unit", "UNITCODE3", units.get(0).getUnitcode());
+        assertEquals("Incorrect last unit", "UNITCODE5", units.get(2).getUnitcode());
+    }
+
+    @Test
+    public void testGetUnitCodesNotTheseUnitCodesPlusUnitCode() {
+
+        Unit unit = new Unit();
+        // required fields
+        unit.setUnitcode("UNITCODE5");
+        unit.setName("za");
+        unit.setShortname("nam5");
+        // not required
+        unit.setUnituser("user5");
+
+        unitDao.save(unit);
+
+        List<String> unitCodes = new ArrayList<String>();
+        unitCodes.add("UNITCODE3");
+        String[] notTheseUnitCodes = new String[] {"UNITCODE2", "UNITCODE4"};
+        String[] plusUnitCodes = new String[] {"UNITCODE5"};
+
+        List<Unit> units = unitDao.get(unitCodes, notTheseUnitCodes, plusUnitCodes);
+
+        assertEquals("Incorrect number of units", 2, units.size());
+        assertEquals("Incorrect first unit", "UNITCODE3", units.get(0).getUnitcode());
+        assertEquals("Incorrect last unit", "UNITCODE5", units.get(1).getUnitcode());
     }
 }
