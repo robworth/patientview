b258b2300d3702572f60f3f810fec654f75baa60
David Roberts <david@solidstategroup.com>
2012-07-24 15:58:27 +0100

    implemented methods

--- patientview/src/main/java/com/worthsoln/repository/impl/EmailVerificationDaoImpl.java
+++ patientview/src/main/java/com/worthsoln/repository/impl/EmailVerificationDaoImpl.java
@@ -1,37 +1,42 @@
 package com.worthsoln.repository.impl;
 
 import com.worthsoln.patientview.model.EmailVerification;
+import com.worthsoln.patientview.model.EmailVerification_;
 import com.worthsoln.repository.AbstractHibernateDAO;
 import com.worthsoln.repository.EmailVerificationDao;
 
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.Calendar;
 import java.util.List;
 
-/**
- *
- */
 public class EmailVerificationDaoImpl extends AbstractHibernateDAO<EmailVerification> implements EmailVerificationDao {
 
     @Override
     public List<EmailVerification> get(String verificationCode) {
 
-//        Session session = HibernateUtil.currentSession();
-//        Transaction tx = session.beginTransaction();
-//        List emailVerifications = session.find("from " + EmailVerification.class.getName() + " as emailverification " +
-//                " where emailverification.verificationcode = ? " +
-//                " and emailverification.expirydatestamp > current_date", verificationCode, Hibernate.STRING);
-//        tx.commit();
-//        HibernateUtil.closeSession();
+        CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<EmailVerification> criteria = builder.createQuery(EmailVerification.class);
+        Root<EmailVerification> emailVerificationRoot = criteria.from(EmailVerification.class);
 
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        // TODO: not sure of the greater than and just using Calendar.getInstance()
+        criteria.where(builder.equal(emailVerificationRoot.get(EmailVerification_.verificationcode), verificationCode),
+                builder.greaterThan(emailVerificationRoot.get(EmailVerification_.expirydatestamp),
+                        Calendar.getInstance()));
+
+        return getEntityManager().createQuery(criteria).getResultList();
     }
 
     @Override
     public void delete(String username) {
+        // I assume there would be only one in there but creating the statement instead of pulling back
+        // all the objects with that username and deleting each one individually
+        Query query = getEntityManager().createNativeQuery("DELETE FROM " + EmailVerification.class.getSimpleName()
+                + " WHERE " + EmailVerification_.username.getName() + " = :" + EmailVerification_.username.getName());
 
-//        Session session = HibernateUtil.currentSession();
-//        Transaction tx = session.beginTransaction();
-//        session.delete("from " + EmailVerification.class.getName() + " as emailverification " +
-//                " where emailverification.username = ?", username, Hibernate.STRING);
-//        tx.commit();
+        query.setParameter(EmailVerification_.username.getName(), username);
+        query.executeUpdate();
     }
 }
