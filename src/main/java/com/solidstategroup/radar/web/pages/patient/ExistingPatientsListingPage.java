813cb9b39d61a6f9f0fc22ef8ecf6c1e567dfe8d
David Roberts <david@solidstategroup.com>
2012-07-27 12:27:22 +0100

    using new getDianogisName method in the diagnosisManager
    this caters for both generic and normal diagnosis

--- src/main/java/com/solidstategroup/radar/web/pages/patient/ExistingPatientsListingPage.java
+++ src/main/java/com/solidstategroup/radar/web/pages/patient/ExistingPatientsListingPage.java
@@ -2,7 +2,6 @@ package com.solidstategroup.radar.web.pages.patient;
 
 import com.solidstategroup.radar.model.Centre;
 import com.solidstategroup.radar.model.Demographics;
-import com.solidstategroup.radar.model.DiagnosisCode;
 import com.solidstategroup.radar.model.user.ProfessionalUser;
 import com.solidstategroup.radar.model.user.User;
 import com.solidstategroup.radar.service.DemographicsManager;
@@ -10,7 +9,6 @@ import com.solidstategroup.radar.service.DiagnosisManager;
 import com.solidstategroup.radar.web.RadarApplication;
 import com.solidstategroup.radar.web.RadarSecuredSession;
 import com.solidstategroup.radar.web.dataproviders.DemographicsDataProvider;
-import com.solidstategroup.radar.web.models.RadarModelFactory;
 import com.solidstategroup.radar.web.pages.BasePage;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebSession;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
@@ -19,8 +17,6 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 @AuthorizeInstantiation({User.ROLE_PROFESSIONAL, User.ROLE_SUPER_USER})
@@ -28,9 +24,9 @@ public class ExistingPatientsListingPage extends BasePage {
 
     @SpringBean
     private DemographicsManager demographicsManager;
+
     @SpringBean
     private DiagnosisManager diagnosisManager;
-
 
     public ExistingPatientsListingPage() {
 
@@ -55,6 +51,7 @@ public class ExistingPatientsListingPage extends BasePage {
             protected void populateItem(Item<Demographics> item) {
                 // Populate fields
                 Demographics demographics = item.getModelObject();
+
                 if (demographics.isGeneric()) {
                     item.add(new BookmarkablePageLink<GenericPatientPage>("edit", GenericPatientPage.class,
                             GenericPatientPage.getPageParameters(demographics)));
@@ -67,13 +64,7 @@ public class ExistingPatientsListingPage extends BasePage {
                 item.add(new Label("surname"), new Label("forename"));
                 item.add(DateLabel.forDatePattern("dateOfBirth", RadarApplication.DATE_PATTERN2));
                 item.add(new Label("id"));
-
-                IModel<DiagnosisCode> diagnosisCodeModel = RadarModelFactory.getDiagnosisCodeModel(new Model<Long>(
-                        item.getModelObject().getId()), diagnosisManager);
-
-                DiagnosisCode diagnosisCode = diagnosisCodeModel.getObject();
-
-                item.add(new Label("diagnosis", diagnosisCode != null ? diagnosisCode.getAbbreviation() : null));
+                item.add(new Label("diagnosis", diagnosisManager.getDiagnosisName(demographics)));
                 item.add(new Label("nhsNumber"));
                 item.add(new Label("hospitalNumber"));
                 item.add(DateLabel.forDatePattern("dateRegistered", RadarApplication.DATE_PATTERN2));
