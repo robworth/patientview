494856543e0a42079e7a9d43a630a20a7ba33d70
David Roberts <david@solidstategroup.com>
2012-08-02 09:32:36 +0100

    added scale items list

--- patientview/src/main/java/com/worthsoln/ibd/action/BaseAction.java
+++ patientview/src/main/java/com/worthsoln/ibd/action/BaseAction.java
@@ -1,5 +1,6 @@
 package com.worthsoln.ibd.action;
 
+import com.worthsoln.ibd.model.enums.AreaToDiscuss;
 import com.worthsoln.ibd.model.enums.BodyPartAffected;
 import com.worthsoln.ibd.model.enums.Complication;
 import com.worthsoln.ibd.model.enums.Diagnosis;
@@ -8,27 +9,39 @@ import com.worthsoln.ibd.model.enums.FamilyHistory;
 import com.worthsoln.ibd.model.enums.Smoking;
 import com.worthsoln.ibd.model.enums.Surgery;
 import com.worthsoln.ibd.model.enums.VaccinationRecord;
+import com.worthsoln.patientview.model.User;
+import com.worthsoln.patientview.model.UserMapping;
+import com.worthsoln.patientview.user.UserUtils;
 import com.worthsoln.service.ibd.IbdManager;
+import com.worthsoln.utils.LegacySpringUtils;
 import org.springframework.web.struts.ActionSupport;
 
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BaseAction extends ActionSupport {
 
-    protected static String SUCCESS = "success";
-    protected static String INPUT = "input";
-    protected static String ERROR = "error";
+    protected static final String SUCCESS = "success";
+    protected static final String INPUT = "input";
+    protected static final String ERROR = "error";
 
-    protected static String SUBMIT_PROPERTY = "submit";
-    protected static String DISEASE_EXTENT_LIST_PROPERTY = "diseaseExtentList";
-    protected static String DIAGNOSIS_LIST_PROPERTY = "diagnosisList";
-    protected static String COMPLICATION_LIST_PROPERTY = "complicationList";
-    protected static String FAMILY_HISTORY_LIST_PROPERTY = "familyHistoryList";
-    protected static String SMOKING_LIST_PROPERTY = "smokingList";
-    protected static String SURGERY_LIST_PROPERTY = "surgeryList";
-    protected static String BODY_PART_AFFECTED_LIST_PROPERTY = "bodyPartAffectedList";
-    protected static String VACCINATION_RECORD_LIST_PROPERTY = "vaccinationRecordList";
+    // my ibd lists
+    protected static final String DISEASE_EXTENT_LIST_PROPERTY = "diseaseExtentList";
+    protected static final String DIAGNOSIS_LIST_PROPERTY = "diagnosisList";
+    protected static final String COMPLICATION_LIST_PROPERTY = "complicationList";
+    protected static final String FAMILY_HISTORY_LIST_PROPERTY = "familyHistoryList";
+    protected static final String SMOKING_LIST_PROPERTY = "smokingList";
+    protected static final String SURGERY_LIST_PROPERTY = "surgeryList";
+    protected static final String BODY_PART_AFFECTED_LIST_PROPERTY = "bodyPartAffectedList";
+    protected static final String VACCINATION_RECORD_LIST_PROPERTY = "vaccinationRecordList";
+
+    // care plan lists
+    protected static final String AREA_TO_DISCUSS_LIST_PROPERTY = "areaToDiscussList";
+    protected static final String SCALE_LIST_PROPERTY = "scaleList";
+
+    protected static List<ScaleItem> scaleList;
 
     protected List<DiseaseExtent> getDiseaseExtentList() {
         return DiseaseExtent.getAsList();
@@ -62,7 +75,50 @@ public class BaseAction extends ActionSupport {
         return VaccinationRecord.getAsList();
     }
 
+    protected List<AreaToDiscuss> getAreaToDiscussList() {
+        return AreaToDiscuss.getAsList();
+    }
+
+    protected List<ScaleItem> getScaleList() {
+        if (scaleList == null) {
+            scaleList = new ArrayList<ScaleItem>();
+
+            for (int x = 1; x <= 10; x++) {
+                scaleList.add(new ScaleItem(x));
+            }
+        }
+
+        return scaleList;
+    }
+
+    protected String getNhsNoForUser(HttpServletRequest request) {
+        User user = UserUtils.retrieveUser(request);
+
+        UserMapping userMapping = LegacySpringUtils.getUserManager().getUserMappingPatientEntered(user);
+
+        if (userMapping != null) {
+            return userMapping.getNhsno();
+        }
+
+        return null;
+    }
+
     protected IbdManager getIbdManager() {
         return getWebApplicationContext().getBean(IbdManager.class);
+    }
+
+    /**
+     * This is just a simple class as the struts list cant just take an array of ints
+     */
+    public class ScaleItem {
+        private int value;
+
+        public ScaleItem(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 }
