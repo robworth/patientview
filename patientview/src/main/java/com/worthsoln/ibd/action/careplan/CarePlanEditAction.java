a6679669211be617be5da99ac6c33c163d000644
David Roberts <david@solidstategroup.com>
2012-08-02 11:25:09 +0100

    added review date to form

--- patientview/src/main/java/com/worthsoln/ibd/action/careplan/CarePlanEditAction.java
+++ patientview/src/main/java/com/worthsoln/ibd/action/careplan/CarePlanEditAction.java
@@ -31,7 +31,14 @@ public class CarePlanEditAction extends BaseAction {
         dynaForm.set(Ibd.BARRIERS_PARAM, carePlan.getBarriers());
         dynaForm.set(Ibd.WHAT_CAN_BE_DONE_PARAM, carePlan.getWhatCanBeDone());
         dynaForm.set(Ibd.CONFIDENCE_SCALE_PARAM, carePlan.getConfidenceScale());
-        // TODO: review date
+
+        String reviewDate = "";
+
+        if (carePlan.getReviewDate() != null) {
+            reviewDate = Ibd.DATE_FORMAT.format(carePlan.getReviewDate());
+        }
+
+        dynaForm.set(Ibd.REVIEW_DATE_PARAM, reviewDate);
 
         Long[] areaToDiscussIds = new Long[carePlan.getAreaToDiscussIds().size()];
         int count = 0;
