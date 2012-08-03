a87b5b4ce0c88572235f313992d1c83bab2ae062
David Roberts <david@solidstategroup.com>
2012-08-03 17:40:32 +0100

    ibd display page

---
+++ patientview/src/main/java/com/worthsoln/ibd/model/medication/MyMedicine.java
@@ -0,0 +1,134 @@
+package com.worthsoln.ibd.model.medication;
+
+import com.worthsoln.ibd.model.medication.enums.MedicationFrequency;
+import com.worthsoln.ibd.model.medication.enums.MedicationNoOf;
+import com.worthsoln.patientview.model.BaseModel;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.util.Date;
+
+@Entity
+@Table(name = "ibd_medicines")
+public class MyMedicine extends BaseModel {
+
+    @Column(nullable = false)
+    private String nhsno;
+
+    @Column(nullable = false)
+    private Date dateStarted;
+
+    @Column(nullable = true)
+    private Date dateStopped;
+
+    @Column(nullable = false)
+    private MedicationType medicationType;
+
+    @Column(nullable = false)
+    private Medication medication;
+
+    @Column(nullable = false)
+    private MedicationDose medicationDose;
+
+    @Transient
+    private MedicationNoOf medicationNoOf;
+
+    @Transient
+    private MedicationFrequency medicationFrequency;
+
+    public String getNhsno() {
+        return nhsno;
+    }
+
+    public void setNhsno(String nhsno) {
+        this.nhsno = nhsno;
+    }
+
+    public Date getDateStarted() {
+        return dateStarted;
+    }
+
+    public void setDateStarted(Date dateStarted) {
+        this.dateStarted = dateStarted;
+    }
+
+    public Date getDateStopped() {
+        return dateStopped;
+    }
+
+    public void setDateStopped(Date dateStopped) {
+        this.dateStopped = dateStopped;
+    }
+
+    public MedicationType getMedicationType() {
+        return medicationType;
+    }
+
+    public void setMedicationType(MedicationType medicationType) {
+        this.medicationType = medicationType;
+    }
+
+    public Medication getMedication() {
+        return medication;
+    }
+
+    public void setMedication(Medication medication) {
+        this.medication = medication;
+    }
+
+    public MedicationDose getMedicationDose() {
+        return medicationDose;
+    }
+
+    public void setMedicationDose(MedicationDose medicationDose) {
+        this.medicationDose = medicationDose;
+    }
+
+    public MedicationNoOf getMedicationNoOf() {
+        return medicationNoOf;
+    }
+
+    public void setMedicationNoOf(MedicationNoOf medicationNoOf) {
+        this.medicationNoOf = medicationNoOf;
+    }
+
+    @Access(AccessType.PROPERTY)
+    @Column(name = "medication_no_of_id", nullable = false)
+    public Long getMedicationNoOfId() {
+        if (medicationNoOf != null) {
+            return medicationNoOf.getId();
+        }
+
+        return null;
+    }
+
+    public void setMedicationNoOfId(Long id) {
+        this.medicationNoOf = MedicationNoOf.getMedicineNoOf(id);
+    }
+
+    public MedicationFrequency getMedicationFrequency() {
+        return medicationFrequency;
+    }
+
+    public void setMedicationFrequency(MedicationFrequency medicationFrequency) {
+        this.medicationFrequency = medicationFrequency;
+    }
+
+    @Access(AccessType.PROPERTY)
+    @Column(name = "medication_frequency_id", nullable = false)
+    public Long getMedicationFrequencyId() {
+        if (medicationFrequency != null) {
+            return medicationFrequency.getId();
+        }
+
+        return null;
+    }
+
+    public void setMedicationFrequencyId(Long id) {
+        this.medicationFrequency = MedicationFrequency.getMedicineFrequency(id);
+    }
+}
