f879c4cc60a77ca8659c254c7933e7f68c3b7508
David Roberts <david@solidstategroup.com>
2012-08-06 15:34:35 +0100

    implemented

--- patientview/src/main/java/com/worthsoln/repository/ibd/MyMedicineDao.java
+++ patientview/src/main/java/com/worthsoln/repository/ibd/MyMedicineDao.java
@@ -4,10 +4,18 @@ import com.worthsoln.ibd.model.medication.MyMedicine;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional(propagation = Propagation.MANDATORY)
 public interface MyMedicineDao {
 
     MyMedicine get(Long id);
 
     void save(MyMedicine myMedicine);
+
+    List<MyMedicine> getAllMedicines(String nhsno);
+
+    List<MyMedicine> getCurrentMedicines(String nhsno);
+
+    List<MyMedicine> getStoppedMedicines(String nhsno);
 }
