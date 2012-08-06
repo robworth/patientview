f450058f1477493ab0c6d9064dd2587e0999ffe8
David Roberts <david@solidstategroup.com>
2012-08-06 15:33:55 +0100

    added tests for myMedicineDao

--- patientview/src/test/java/com/worthsoln/test/repository/ibd/MyMedicineDaoTest.java
+++ patientview/src/test/java/com/worthsoln/test/repository/ibd/MyMedicineDaoTest.java
@@ -78,6 +78,138 @@ public class MyMedicineDaoTest extends BaseDaoTest {
         assertTrue("Medicine not stopped", checkMyMedicine.hasStopped());
     }
 
+    @Test
+    public void testGetAllMedicines() throws Exception {
+        MedicationType medicationType = getTestMedicationTypeObject();
+        Medication medication = getTestMedicationObject();
+        MedicationDose medicationDose = medication.getAllowedDosages().get(0);
+
+        // create two test medicines
+        MyMedicine myMedicine1 = new MyMedicine();
+
+        myMedicine1.setNhsno("123456789");
+        myMedicine1.setDateStarted(new Date());
+        myMedicine1.setDateStopped(new Date());
+        myMedicine1.setMedicationType(medicationType);
+        myMedicine1.setMedication(medication);
+        myMedicine1.setMedicationDose(medicationDose);
+        myMedicine1.setMedicationNoOf(MedicationNoOf.ONE_TO_FOUR);
+        myMedicine1.setMedicationFrequency(MedicationFrequency.ONCE_A_DAY);
+        myMedicine1.setReasonForStopping("Reason for stopping");
+
+        myMedicineDao.save(myMedicine1);
+
+        assertTrue("Invalid id for new medicine 1", myMedicine1.getId() > 0);
+
+        MyMedicine myMedicine2 = new MyMedicine();
+
+        myMedicine2.setNhsno("123456789");
+        myMedicine2.setDateStarted(new Date());
+        myMedicine2.setDateStopped(new Date());
+        myMedicine2.setMedicationType(medicationType);
+        myMedicine2.setMedication(medication);
+        myMedicine2.setMedicationDose(medicationDose);
+        myMedicine2.setMedicationNoOf(MedicationNoOf.ONE_TO_FOUR);
+        myMedicine2.setMedicationFrequency(MedicationFrequency.ONCE_A_DAY);
+        myMedicine2.setReasonForStopping("Reason for stopping");
+
+        myMedicineDao.save(myMedicine2);
+
+        assertTrue("Invalid id for new medicine 2", myMedicine2.getId() > 0);
+
+        // now try to pull back all medicines for this user and should see 2
+        List<MyMedicine> checkMyMedicines = myMedicineDao.getAllMedicines("123456789");
+
+        assertTrue("No medicines found", !checkMyMedicines.isEmpty() && checkMyMedicines.size() > 0);
+        assertTrue("To many medicines found", checkMyMedicines.size() == 2);
+    }
+
+    @Test
+    public void testGetCurrentMedicines() throws Exception {
+        MedicationType medicationType = getTestMedicationTypeObject();
+        Medication medication = getTestMedicationObject();
+        MedicationDose medicationDose = medication.getAllowedDosages().get(0);
+
+        // create two test medicines 1 that does not have a date stopped meaning current and one which has
+        MyMedicine myMedicine1 = new MyMedicine();
+
+        myMedicine1.setNhsno("123456789");
+        myMedicine1.setDateStarted(new Date());
+        myMedicine1.setMedicationType(medicationType);
+        myMedicine1.setMedication(medication);
+        myMedicine1.setMedicationDose(medicationDose);
+        myMedicine1.setMedicationNoOf(MedicationNoOf.ONE_TO_FOUR);
+        myMedicine1.setMedicationFrequency(MedicationFrequency.ONCE_A_DAY);
+
+        myMedicineDao.save(myMedicine1);
+
+        assertTrue("Invalid id for new medicine 1", myMedicine1.getId() > 0);
+
+        MyMedicine myMedicine2 = new MyMedicine();
+
+        myMedicine2.setNhsno("123456789");
+        myMedicine2.setDateStarted(new Date());
+        myMedicine2.setDateStopped(new Date());
+        myMedicine2.setMedicationType(medicationType);
+        myMedicine2.setMedication(medication);
+        myMedicine2.setMedicationDose(medicationDose);
+        myMedicine2.setMedicationNoOf(MedicationNoOf.ONE_TO_FOUR);
+        myMedicine2.setMedicationFrequency(MedicationFrequency.ONCE_A_DAY);
+        myMedicine2.setReasonForStopping("Reason for stopping");
+
+        myMedicineDao.save(myMedicine2);
+
+        // now try to pull back all medicines for this user and should see 1 with the id of the first
+        List<MyMedicine> checkMyMedicines = myMedicineDao.getCurrentMedicines("123456789");
+
+        assertTrue("No medicines found", !checkMyMedicines.isEmpty() && checkMyMedicines.size() > 0);
+        assertTrue("To many medicines found", checkMyMedicines.size() == 1);
+        assertEquals("Wrong id for medicine pulled back", checkMyMedicines.get(0).getId(), myMedicine1.getId());
+    }
+
+    @Test
+    public void testGetStoppedMedicines() throws Exception {
+        MedicationType medicationType = getTestMedicationTypeObject();
+        Medication medication = getTestMedicationObject();
+        MedicationDose medicationDose = medication.getAllowedDosages().get(0);
+
+        // create two test medicines 1 that does not have a date stopped meaning current and one which has
+        MyMedicine myMedicine1 = new MyMedicine();
+
+        myMedicine1.setNhsno("123456789");
+        myMedicine1.setDateStarted(new Date());
+        myMedicine1.setMedicationType(medicationType);
+        myMedicine1.setMedication(medication);
+        myMedicine1.setMedicationDose(medicationDose);
+        myMedicine1.setMedicationNoOf(MedicationNoOf.ONE_TO_FOUR);
+        myMedicine1.setMedicationFrequency(MedicationFrequency.ONCE_A_DAY);
+
+        myMedicineDao.save(myMedicine1);
+
+        assertTrue("Invalid id for new medicine 1", myMedicine1.getId() > 0);
+
+        MyMedicine myMedicine2 = new MyMedicine();
+
+        myMedicine2.setNhsno("123456789");
+        myMedicine2.setDateStarted(new Date());
+        myMedicine2.setDateStopped(new Date());
+        myMedicine2.setMedicationType(medicationType);
+        myMedicine2.setMedication(medication);
+        myMedicine2.setMedicationDose(medicationDose);
+        myMedicine2.setMedicationNoOf(MedicationNoOf.ONE_TO_FOUR);
+        myMedicine2.setMedicationFrequency(MedicationFrequency.ONCE_A_DAY);
+        myMedicine2.setReasonForStopping("Reason for stopping");
+
+        myMedicineDao.save(myMedicine2);
+
+        // now try to pull back all medicines for this user and should see 1 with the id of the second
+        List<MyMedicine> checkMyMedicines = myMedicineDao.getStoppedMedicines("123456789");
+
+        assertTrue("No medicines found", !checkMyMedicines.isEmpty() && checkMyMedicines.size() > 0);
+        assertTrue("To many medicines found", checkMyMedicines.size() == 1);
+        assertEquals("Wrong id for medicine pulled back", checkMyMedicines.get(0).getId(), myMedicine2.getId());
+    }
+
     private MedicationType getTestMedicationTypeObject() {
         MedicationType medicationType = new MedicationType();
         medicationType.setName("Suppository");
