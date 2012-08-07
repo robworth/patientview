0157f9707143b02b0b5cce772db56e67e2d0f715
David Roberts <david@solidstategroup.com>
2012-08-07 17:58:52 +0100

    not lazy loading

--- patientview/src/main/java/com/worthsoln/ibd/model/medication/MedicationType.java
+++ patientview/src/main/java/com/worthsoln/ibd/model/medication/MedicationType.java
@@ -1,6 +1,8 @@
 package com.worthsoln.ibd.model.medication;
 
 import com.worthsoln.patientview.model.BaseModel;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -19,6 +21,7 @@ public class MedicationType extends BaseModel {
     private String name;
 
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @LazyCollection(LazyCollectionOption.FALSE)
     @JoinTable(
             name = "ibd_medication_type_medications",
             joinColumns = {@JoinColumn(name = "medication_type_id")},
