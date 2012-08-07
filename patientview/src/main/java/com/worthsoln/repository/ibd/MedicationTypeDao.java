2e5b050b30e119e771efc17774721e5003da203d
David Roberts <david@solidstategroup.com>
2012-08-07 18:00:39 +0100

    renamed and implemented

--- patientview/src/main/java/com/worthsoln/repository/ibd/MedicationTypeDao.java
+++ patientview/src/main/java/com/worthsoln/repository/ibd/MedicationTypeDao.java
@@ -4,10 +4,14 @@ import com.worthsoln.ibd.model.medication.MedicationType;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional(propagation = Propagation.MANDATORY)
 public interface MedicationTypeDao {
 
     MedicationType get(Long id);
+
+    List<MedicationType> getAll();
 
     void save(MedicationType medication);
 }
