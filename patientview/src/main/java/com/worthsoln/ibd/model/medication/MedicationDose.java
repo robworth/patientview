b995e12eea4094048a00de79085ede4c49395bc4
David Roberts <david@solidstategroup.com>
2012-08-09 12:35:18 +0100

    formatting the value of mg to Grams and KG if needed

--- patientview/src/main/java/com/worthsoln/ibd/model/medication/MedicationDose.java
+++ patientview/src/main/java/com/worthsoln/ibd/model/medication/MedicationDose.java
@@ -16,8 +16,27 @@ public class MedicationDose extends BaseModel {
     @Column(nullable = true)
     private String extraInformation;
 
-    public String getMgValueAsString() {
-        return mg.toString();
+    public String getFormattedValue() {
+        String doseValueAsString = "";
+
+        /**
+         * If the mg is grt OR eq to 1,000,000 then convert to KG
+         * If the mg is grt OR eq to 1,000 then convert to Grams
+         * Else just leave as mg
+         */
+        if (mg >= 1000000) {
+            doseValueAsString = (mg / (1000 * 1000)) + "Kg";
+        } else if (mg >= 1000) {
+            doseValueAsString = (mg / 1000) + "G";
+        } else {
+            doseValueAsString = mg + "mg";
+        }
+
+        if (extraInformation != null && extraInformation.length() > 0) {
+            doseValueAsString += extraInformation;
+        }
+
+        return doseValueAsString;
     }
 
     public Double getMg() {
