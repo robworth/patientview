8acb911c8e9236334d3ee219469d705a1189a87b
David Roberts <david@solidstategroup.com>
2012-08-13 18:22:24 +0100

    implemented

--- src/main/java/com/solidstategroup/radar/service/alport/impl/MedicineManagerImpl.java
+++ src/main/java/com/solidstategroup/radar/service/alport/impl/MedicineManagerImpl.java
@@ -1,6 +1,32 @@
 package com.solidstategroup.radar.service.alport.impl;
 
+import com.solidstategroup.radar.dao.alport.MedicineDao;
+import com.solidstategroup.radar.model.alport.Medicine;
 import com.solidstategroup.radar.service.alport.MedicineManager;
 
+import java.util.List;
+
 public class MedicineManagerImpl implements MedicineManager {
+
+    private MedicineDao medicineDao;
+
+    public void save(Medicine medicine) {
+        medicineDao.save(medicine);
+    }
+
+    public void delete(Medicine medicine) {
+        medicineDao.delete(medicine);
+    }
+
+    public Medicine get(Long id) {
+        return medicineDao.get(id);
+    }
+
+    public List<Medicine> getMedicines(String nhsNo) {
+        return medicineDao.getMedicines(nhsNo);
+    }
+
+    public void setMedicineDao(MedicineDao medicineDao) {
+        this.medicineDao = medicineDao;
+    }
 }
