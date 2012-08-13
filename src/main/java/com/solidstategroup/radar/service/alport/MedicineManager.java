1e73ea6db04a28c40637610ec5ccf4fc01f8abd6
David Roberts <david@solidstategroup.com>
2012-08-13 18:22:24 +0100

    implemented

--- src/main/java/com/solidstategroup/radar/service/alport/MedicineManager.java
+++ src/main/java/com/solidstategroup/radar/service/alport/MedicineManager.java
@@ -1,4 +1,16 @@
 package com.solidstategroup.radar.service.alport;
 
+import com.solidstategroup.radar.model.alport.Medicine;
+
+import java.util.List;
+
 public interface MedicineManager {
+
+    void save(Medicine medicine);
+
+    void delete(Medicine medicine);
+
+    Medicine get(Long id);
+
+    List<Medicine> getMedicines(String nhsNo);
 }
