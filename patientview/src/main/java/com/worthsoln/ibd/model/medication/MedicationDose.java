0e335c28f62064aeb5e5e515d445727f016e91fe
David Roberts <david@solidstategroup.com>
2012-09-06 12:04:50 +0100

    formatting the mg so it doesnt show .0

--- patientview/src/main/java/com/worthsoln/ibd/model/medication/MedicationDose.java
+++ patientview/src/main/java/com/worthsoln/ibd/model/medication/MedicationDose.java
@@ -5,16 +5,25 @@ import com.worthsoln.patientview.model.BaseModel;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.text.DecimalFormat;
 
 @Entity
 @Table(name = "ibd_medication_dose")
 public class MedicationDose extends BaseModel {
+
+    @Transient
+    private static final DecimalFormat format = new DecimalFormat();
 
     @Column(nullable = true)
     private Double mg;
 
     @Column(nullable = true)
     private String extraInformation;
+
+    public MedicationDose() {
+        format.setDecimalSeparatorAlwaysShown(false);
+    }
 
     public String getFormattedValue() {
         String doseValueAsString = "";
@@ -26,11 +35,11 @@ public class MedicationDose extends BaseModel {
              * Else just leave as mg
              */
             if (mg >= 1000000) {
-                doseValueAsString = (mg / (1000 * 1000)) + "Kg";
+                doseValueAsString = format.format((mg / (1000 * 1000))) + "Kg";
             } else if (mg >= 1000) {
-                doseValueAsString = (mg / 1000) + "G";
+                doseValueAsString = format.format((mg / 1000)) + "G";
             } else {
-                doseValueAsString = mg + "mg";
+                doseValueAsString = format.format(mg) + "mg";
             }
         }
 
