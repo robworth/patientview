76a7e85bd1363c516a52d4653ebba34009c37293
David Roberts <david@solidstategroup.com>
2012-09-06 16:16:52 +0100

    new import data

--- patientview/src/sql/mysql/ibd-only-build/1-ibd-only-build.sql
+++ patientview/src/sql/mysql/ibd-only-build/1-ibd-only-build.sql
@@ -1,3 +1,24 @@
 delete from tenancy where id = 1;
 
 -- various delete calls
+
+
+-- my ibd links data
+
+
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Primary Diagnosis','myIbdLinks','Primary Diagnosis','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Disease Extent','myIbdLinks','Disease Extent','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Year of Diagnosis','myIbdLinks','Year of Diagnosis','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Complications','myIbdLinks','Complications','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Other parts of the body affected','myIbdLinks','Other parts of the body affected','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Year for Surveillance Colonoscopy','myIbdLinks','Year for Surveillance Colonoscopy','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Named Consultant','myIbdLinks','Named Consultant','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Nurses','myIbdLinks','Nurses','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Weight','myIbdLinks','Weight','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('IBD Related Family History','myIbdLinks','IBD Related Family History','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Smoking History','myIbdLinks','Smoking History','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Surgery History','myIbdLinks','Surgery History','','','1');
+insert into `edtacode` (`edtaCode`, `linkType`, `description`, `medicalLink01`, `medicalLink02`, `tenancy_id`) values('Vaccination History','myIbdLinks','Vaccination History','','','1');
+
+
+
