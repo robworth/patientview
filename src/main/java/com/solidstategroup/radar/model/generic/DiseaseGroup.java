cb6da8ebf2b994d3376163e118534780fa10ef8e
deniz@solidstategroup.com <deniz@solidstategroup.com>
2012-10-01 11:46:41 +0100

    10 new groups

--- src/main/java/com/solidstategroup/radar/model/generic/DiseaseGroup.java
+++ src/main/java/com/solidstategroup/radar/model/generic/DiseaseGroup.java
@@ -10,6 +10,17 @@ public class DiseaseGroup implements Serializable, Comparable<DiseaseGroup> {
     public static final String SRNS_DISEASE_GROUP_ID = "SRNS";
     public static final String MPGN_DISEASEGROUP_ID = "MPGN";
     public static final String ALPORT_DISEASEGROUP_ID = "ALPORT";
+    public static final String HNF1B_DISEASEGROUP_ID = "HNF1B";
+    public static final String AHUS_DISEASEGROUP_ID = "AHUS";
+    public static final String ARPKD_DISEASEGROUP_ID = "ARPKD";
+    public static final String VASRDG_DISEASEGROUP_ID = "VASRDG";
+    public static final String HYPALK_DISEASEGROUP_ID = "HYPALK";
+    public static final String CYSTIN_DISEASEGROUP_ID = "CYSTIN";
+    public static final String STECHUS_DISEASEGROUP_ID = "STECHUS";
+    public static final String HYPERRDG_DISEASEGROUP_ID = "HYPERRDG";
+    public static final String FUAN_DISEASEGROUP_ID = "FUAN";
+    public static final String MEMRDG_DISEASEGROUP_ID = "MEMRDG";
+
     private String id;
     private String name;
     private String shortName;
