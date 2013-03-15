bca8e016a58e3cb51f41d6f824342eeaacc33462
David Roberts <david@solidstategroup.com>
2013-03-15 17:22:32 +0000

    added in messaging tables

--- src/main/resources/sql/features/7/7.sql
+++ src/main/resources/sql/features/7/7.sql
@@ -4,22 +4,21 @@ CREATE TABLE `conversation` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `deleted` tinyint(1) NOT NULL,
   `started` datetime NOT NULL,
-  `from_user_id` bigint(20) NOT NULL,
-  `to_user_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK35E930A32BF31DAE` (`to_user_id`),
-  KEY `FK35E930A39C9A87DD` (`from_user_id`),
-  CONSTRAINT `FK35E930A39C9A87DD` FOREIGN KEY (`from_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FK35E930A32BF31DAE` FOREIGN KEY (`to_user_id`) REFERENCES `user` (`id`)
-)
+  `participant1_id` bigint(20) NOT NULL,
+  `participant2_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 CREATE TABLE `message` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
   `date` datetime NOT NULL,
   `deleted` tinyint(1) NOT NULL,
-  `description` text NOT NULL,
+  `hasRead` tinyint(1) NOT NULL,
   `conversation_id` bigint(20) NOT NULL,
+  `recipient_id` bigint(20) NOT NULL,
+  `sender_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK9C2397E72776A072` (`conversation_id`),
   CONSTRAINT `FK9C2397E72776A072` FOREIGN KEY (`conversation_id`) REFERENCES `conversation` (`id`)
-)
+);
