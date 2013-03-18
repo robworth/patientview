089b7598e8f9f3e2deabe5307221c4622758f8fd
deniz@solidstategroup.com <deniz@solidstategroup.com>
2013-03-18 13:45:58 +0000

    20 - Radar - Add patient name and DoB to the top of every patient input page.

--- src/main/java/com/solidstategroup/radar/web/panels/PatientDetailPanel.java
+++ src/main/java/com/solidstategroup/radar/web/panels/PatientDetailPanel.java
@@ -3,45 +3,102 @@ package com.solidstategroup.radar.web.panels;
 
 import com.solidstategroup.radar.model.Demographics;
 import com.solidstategroup.radar.web.RadarApplication;
+import org.apache.commons.lang.StringUtils;
+import org.apache.wicket.datetime.PatternDateConverter;
 import org.apache.wicket.datetime.markup.html.form.DateTextField;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
-import java.text.SimpleDateFormat;
+import org.apache.wicket.model.PropertyModel;
+
+import java.util.Date;
 
 public class PatientDetailPanel extends Panel {
-    public PatientDetailPanel(String id, Demographics demographics, String title) {
+    public PatientDetailPanel(String id, final Demographics demographics, String title) {
         super(id);
+
         WebMarkupContainer details = new WebMarkupContainer("details", new CompoundPropertyModel<Object>(demographics));
+        details.setOutputMarkupId(true);
+        details.setOutputMarkupPlaceholderTag(true);
         add(details);
 
         // Note: this panel is shown after initial enter new patient, and you may not have all patient data yet
 
-        // add components
+        /**
+         * Add components
+         */
+
+        // title
         details.add(new Label("title", title));
+
+        // radar number
         TextField<Long> radarNumberField = new TextField<Long>("id");
-        Label diseaseGroup = new Label("diseaseGroup", demographics.getDiseaseGroup().getName());
+        details.add(radarNumberField);
 
-        StringBuilder sb = new StringBuilder();
-        if (demographics.getForename() != null && demographics.getForename().length() > 0) {
-            sb.append(demographics.getForename());
-            if (demographics.getSurname() != null && demographics.getSurname().length() > 0) {
-                sb.append(" ");
+        // disease group
+        Label diseaseGroup = new Label("diseaseGroup", new PropertyModel<Object>(demographics.getDiseaseGroup(),
+                "name"));
+        details.add(diseaseGroup);
+
+        // forename
+        Label nameLabel = new Label("nameLabel", "Patient Name") {
+            @Override
+            public boolean isVisible() {
+                return StringUtils.isNotBlank(demographics.getForename());
             }
-        }
-        if (demographics.getSurname() != null && demographics.getSurname().length() > 0) {
-            sb.append(demographics.getSurname());
-        }
-        details.add(new Label("name", sb.toString()));
+        };
+        nameLabel.setOutputMarkupId(true);
+        nameLabel.setOutputMarkupPlaceholderTag(true);
+        details.add(nameLabel);
 
-        String dob = "";
+        TextField<Long> forename = new TextField<Long>("forename") {
+            @Override
+            public boolean isVisible() {
+                return StringUtils.isNotBlank(demographics.getForename());
+            }
+        };
+        forename.setOutputMarkupId(true);
+        forename.setOutputMarkupPlaceholderTag(true);
+        details.add(forename);
+
+        // surname
+        TextField<Long> surname = new TextField<Long>("surname") {
+            @Override
+            public boolean isVisible() {
+                return StringUtils.isNotBlank(demographics.getSurname());
+            }
+        };
+        surname.setOutputMarkupId(true);
+        surname.setOutputMarkupPlaceholderTag(true);
+        details.add(surname);
+
+        // date registered
         DateTextField dateRegistered = DateTextField.forDatePattern("dateRegistered", RadarApplication.DATE_PATTERN);
-        if (demographics.getDateOfBirth() != null) {
-            dob = new SimpleDateFormat(RadarApplication.DATE_PATTERN).format(demographics.getDateOfBirth());
-        }
-        details.add(new Label("dob", dob));
-        details.add(radarNumberField, diseaseGroup, dateRegistered);
+        details.add(dateRegistered);
+
+        // date of birth
+        Label dobLabel = new Label("dobLabel", "Patient DOB") {
+            @Override
+            public boolean isVisible() {
+                return demographics.getDateOfBirth() != null;
+            }
+        };
+        dobLabel.setOutputMarkupId(true);
+        dobLabel.setOutputMarkupPlaceholderTag(true);
+        details.add(dobLabel);
+
+        DateTextField dateOfBirthTextField = new DateTextField("dob",
+                new PropertyModel<Date>(demographics, "dateOfBirth"), new PatternDateConverter(
+                RadarApplication.DATE_PATTERN, true)) {
+            @Override
+            public boolean isVisible() {
+                return demographics.getDateOfBirth() != null;
+            }
+        };
+        dateOfBirthTextField.setOutputMarkupId(true);
+        dateOfBirthTextField.setOutputMarkupPlaceholderTag(true);
+        details.add(dateOfBirthTextField);
     }
 }
