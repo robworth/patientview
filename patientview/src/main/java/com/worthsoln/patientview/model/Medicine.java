9d46e7401b935a0fc1a99a91dc153dbdbf9ba646
deniz@solidstategroup.com <deniz@solidstategroup.com>
2013-03-26 15:18:17 +0000

    null check on medicine start date

--- patientview/src/main/java/com/worthsoln/patientview/model/Medicine.java
+++ patientview/src/main/java/com/worthsoln/patientview/model/Medicine.java
@@ -96,11 +96,18 @@ public class Medicine extends BaseModel {
     public String getFormattedStartDate() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
         SimpleDateFormat dateTimeFormat = new SimpleDateFormat("dd/MM/yy HH:mm");
-        if ((startdate.get(Calendar.HOUR_OF_DAY) == 0) && (startdate.get(Calendar.MINUTE) == 0)) {
-            return dateFormat.format(startdate.getTime());
-        } else {
-            return dateTimeFormat.format(startdate.getTime());
+
+        String formattedStartDate = "";
+
+        if (startdate != null) {
+            if ((startdate.get(Calendar.HOUR_OF_DAY) == 0) && (startdate.get(Calendar.MINUTE) == 0)) {
+                formattedStartDate = dateFormat.format(startdate.getTime());
+            } else {
+                formattedStartDate = dateTimeFormat.format(startdate.getTime());
+            }
         }
+
+        return formattedStartDate;
     }
 
 }
