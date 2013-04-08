1c955b17169de2f6bbd9af03703546b094477d50
Paul Chenery <paul@solidstategroup.com>
2013-04-08 11:28:35 +0100

    move the drop columns into the next release

--- src/main/resources/sql/features/9/9.sql
+++ src/main/resources/sql/features/9/9.sql
@@ -13,20 +13,3 @@ CREATE TABLE `rdr_user_mapping` (
   constraint `FK_RDR_USER_MAPPING_USERID` foreign key (`userId`) references `user`(`id`) on delete Cascade
 );
 
-/*
-ONLY DO THIS AFTER YOU HAVE RUN THE EXPORT FILE TO MAP THESE FIELDS INTO RPV
-*/
-ALTER TABLE `tbl_adminusers`
-  DROP COLUMN `uName`,
-  DROP COLUMN `uEmail`,
-  DROP COLUMN `uPass`,
-  DROP COLUMN `uUserName`;
-
-ALTER TABLE `tbl_patient_users`
-  DROP COLUMN `pUserName`,
-  DROP COLUMN `pPassWord`;
-
-ALTER TABLE `tbl_users`
-  DROP COLUMN `uEmail`,
-  DROP COLUMN `uPass`,
-  DROP COLUMN `uUserName`;
