5060a52b55929fde2733094f4de0302f02ae0f77
deniz@solidstategroup.com <deniz@solidstategroup.com>
2013-04-17 14:23:24 +0100

    pv_admin_notifications table

---
+++ src/main/resources/sql/features/15/15.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `pv_admin_notification` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `notification_id` bigint(20) NOT NULL,
+  `email` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+);
+
+insert into pv_admin_notification (id, notification_id, email) values (1, 1, 'support@example.com');
+insert into pv_admin_notification (id, notification_id, email) values (2, 1, 'admin@example.com');
+insert into pv_admin_notification (id, notification_id, email) values (3, 2, 'superadmin@example.com');
