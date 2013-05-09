c27e6a86cc57af812d9952884cd7097d289fd7db
Paul Chenery <paul@solidstategroup.com>
2013-05-09 15:02:45 +0100

    correct homepage fail

--- src/main/java/com/solidstategroup/radar/web/pages/HomePage.java
+++ src/main/java/com/solidstategroup/radar/web/pages/HomePage.java
@@ -1,12 +1,8 @@
 package com.solidstategroup.radar.web.pages;
 
 
-import com.solidstategroup.radar.web.pages.content.DiseaseIndexPage;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-
 public class HomePage extends BasePage {
     public HomePage() {
-        add(new BookmarkablePageLink("diseaseIndexLink", DiseaseIndexPage.class));
     }
 
 }
