b639ecec1f24aa5209facd2ace08bb611fa9a4c8
richardli <licy@shinetechchina.com>
2013-05-21 16:34:25 +0800

    65 - PV - Admin can message all patients in unit
    Integrated the spring & quartz

---
+++ patientview/src/main/java/com/worthsoln/job/GetUnsentMessageJob.java
@@ -0,0 +1,11 @@
+package com.worthsoln.job;
+
+/**
+ * Get the unsent messages then insert the records to EamilQueue
+ */
+public class GetUnsentMessageJob {
+
+    public void execute() {
+        // todo
+    }
+}
---
+++ patientview/src/main/java/com/worthsoln/job/SendMailJob.java
@@ -0,0 +1,14 @@
+package com.worthsoln.job;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Send the email to special group members after the unitadmin send the bulk message
+ */
+public class SendMailJob {
+
+    public void execute() {
+        // todo
+    }
+}
