23cd8fc14c9dfeee8e8b8f5d9ac9642f2b6b5969
Paul Chenery <paul@solidstategroup.com>
2013-05-23 12:53:41 +0100

    fix data to work with constraint

--- src/main/resources/sql/features/18/18.sql
+++ src/main/resources/sql/features/18/18.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `PV_ADMIN_NOTIFICATION` ADD UNIQUE KEY (`notification_id`, `email`);
+delete from PV_ADMIN_NOTIFICATION where id = 3;
+ALTER TABLE `PV_ADMIN_NOTIFICATION` ADD PRIMARY KEY (`notification_id`, `email`);
