2b9158bc04388a5fd67278739d5f7a5a04c3ca2f
richardli <licy@shinetechchina.com>
2013-07-10 12:28:48 +0800

    67 - merge tbl_demographics into patient table
    Fixed the insert data from tbl_demographics to patient.

--- patientview-parent/common/src/main/resources/sql/features/21/data.sql
+++ patientview-parent/common/src/main/resources/sql/features/21/data.sql
@@ -1,10 +1,40 @@
 /**
+    Update the patient with tbl_demograpics data
+ */
+UPDATE patient p INNER JOIN tbl_demographics d on p.nhsno = d.nhs_no
+SET p.RADAR_NO = d.RADAR_NO,
+    p.RR_NO = d.RR_NO,
+    p.DATE_REG = d.DATE_REG,
+    p.NHS_NO_TYPE = d.NHS_NO_TYPE,
+    p.UKT_NO = d.UKT_NO,
+    p.SNAME_ALIAS = d.SNAME_ALIAS,
+    p.AGE = d.AGE,
+    p.ETHNIC_GP = d.ETHNIC_GP,
+    p.POSTCODE_OLD = d.POSTCODE_OLD,
+    p.CONSENT = d.CONSENT,
+    p.DATE_BAPN_REG = d.DATE_BAPN_REG,
+    p.CONS_NEPH = d.CONS_NEPH,
+    p.STATUS = d.STATUS,
+    p.emailAddress = d.emailAddress,
+    p.RRT_modality = d.RRT_modality,
+    p.genericDiagnosis = d.genericDiagnosis,
+    p.dateOfGenericDiagnosis = d.dateOfGenericDiagnosis,
+    p.otherClinicianAndContactInfo = d.otherClinicianAndContactInfo,
+    p.comments = d.comments,
+    p.republicOfIrelandId = d.republicOfIrelandId,
+    p.isleOfManId = d.isleOfManId,
+    p.channelIslandsId = d.channelIslandsId,
+    p.indiaId = d.indiaId,
+    p.generic = d.generic;
+
+/**
      Inserts data in tbl_demograpics to patient
  */
-INSERT INTO patient (RADAR_NO, RR_NO, DATE_REG, NHS_NO_TYPE, UKT_NO, SNAME_ALIAS, AGE, ETHNIC_GP, POSTCODE_OLD, CONSENT, DATE_BAPN_REG, CONS_NEPH, STATUS, emailAddress, RRT_modality, otherClinicianAndContactInfo, comments, republicOfIrelandId, isleOfManId, channelIslandsId, indiaId, generic, NHSNO, HOSPITALNUMBER, SURNAME, FORENAME, DATEOFBIRTH, ADDRESS1, ADDRESS2, ADDRESS3, ADDRESS4, POSTCODE, TELEPHONE1, TELEPHONE2, MOBILE)
-SELECT                d.RADAR_NO, d.RR_NO, d.DATE_REG, d.NHS_NO_TYPE, d.UKT_NO, d.SNAME_ALIAS, d.AGE, d.ETHNIC_GP, d.POSTCODE_OLD, d.CONSENT, d.DATE_BAPN_REG, d.CONS_NEPH, d.STATUS, d.emailAddress, d.RRT_modality, d.otherClinicianAndContactInfo, d.comments, d.republicOfIrelandId, d.isleOfManId, d.channelIslandsId, d.indiaId, d.generic, d.NHS_NO, d.HOSP_NO, d.SNAME, d.FNAME, d.DOB, d.ADD1, d.ADD2, d.ADD3, d.ADD4, d.POSTCODE, d.PHONE1, d.PHONE2, d.MOBILE
+INSERT INTO patient (RADAR_NO, RR_NO, DATE_REG, NHS_NO_TYPE, UKT_NO, SNAME_ALIAS, AGE, ETHNIC_GP, POSTCODE_OLD, CONSENT, DATE_BAPN_REG, CONS_NEPH, STATUS, emailAddress, RRT_modality, genericDiagnosis, dateOfGenericDiagnosis, otherClinicianAndContactInfo, comments, republicOfIrelandId, isleOfManId, channelIslandsId, indiaId, generic, NHSNO, HOSPITALNUMBER, SURNAME, FORENAME, DATEOFBIRTH, ADDRESS1, ADDRESS2, ADDRESS3, ADDRESS4, POSTCODE, TELEPHONE1, TELEPHONE2, MOBILE, unitcode)
+SELECT                d.RADAR_NO, d.RR_NO, d.DATE_REG, d.NHS_NO_TYPE, d.UKT_NO, d.SNAME_ALIAS, d.AGE, d.ETHNIC_GP, d.POSTCODE_OLD, d.CONSENT, d.DATE_BAPN_REG, d.CONS_NEPH, d.STATUS, d.emailAddress, d.RRT_modality, d.genericDiagnosis, d.dateOfGenericDiagnosis, d.otherClinicianAndContactInfo, d.comments, d.republicOfIrelandId, d.isleOfManId, d.channelIslandsId, d.indiaId, d.generic, d.NHS_NO, d.HOSP_NO, d.SNAME, d.FNAME, d.DOB, d.ADD1, d.ADD2, d.ADD3, d.ADD4, d.POSTCODE, d.PHONE1, d.PHONE2, d.MOBILE, d.RDG
 FROM tbl_demographics d
-WHERE d.nhs_no in (select distinct d.nhs_no from tbl_demographics d, patient p where p.nhsno != d.nhs_no);
+WHERE d.nhs_no in (SELECT DISTINCT d.nhs_no FROM tbl_demographics d, patient p WHERE p.nhsno != d.nhs_no);
+
 
 /**
     Update the sex
@@ -20,21 +50,6 @@ SET p.sex =
   );
 
 /**
-    Update the centreCode
- */
-
-UPDATE patient p
-SET p.unitcode =
-  (
-    SELECT u.unitcode
-    FROM unit u, tbl_demographics d
-    WHERE d.nhs_no = p.nhsno
-    AND u.id = d.renal_unit
-  )
-WHERE unitcode = "" OR unitcode IS NULL;
-
-
-/**
     Insert data into usermapping with non null value in RENAL_UNIT_2 field of tbl_demograpics
  */
 
@@ -47,8 +62,52 @@ SELECT CONCAT(d.fname, d.sname) AS username,
   ( SELECT DISTINCT um.specialty_id
       FROM usermapping um
      WHERE um.nhsno = d.nhs_no
+       AND um.username NOT LIKE '%-GP%'
+  ) AS specialty_id
+FROM tbl_demographics d
+WHERE d.renal_unit_2 IS NOT NULL
+  AND NOT EXISTS (
+                      SELECT d.*
+                        FROM tbl_demographics d LEFT OUTER JOIN unit u ON d.renal_unit = u.id, usermapping ump
+                        WHERE d.nhs_no = ump.nhsno
+                          AND u.unitcode = ump.unitcode
+                    );
+
+/**
+    Insert into usermapping with tbl_demographics nhs_no and RDG fields
+ */
+INSERT INTO usermapping (username, unitcode, nhsno, specialty_id)
+SELECT CONCAT(d.fname, d.sname) AS username,
+  d.RDG AS unitcode,
+  d.nhs_no AS nhsno,
+  ( SELECT DISTINCT um.specialty_id
+      FROM usermapping um
+     WHERE um.nhsno = d.nhs_no
+       AND um.username NOT LIKE '%-GP%'
+  ) AS specialty_id
+FROM tbl_demographics d, unit u, patient p
+WHERE d.RDG = u.unitcode
+  AND P.unitcode = u.unitcode
+  AND p.nhsno = d.nhs_no
+  AND u.sourceType = 'radargroup';
+
+/**
+    Insert into usermapping with use renal_unit
+ */
+INSERT INTO usermapping (username, unitcode, nhsno, specialty_id)
+SELECT CONCAT(d.fname, d.sname) AS username,
+  ( SELECT u.unitcode
+      FROM unit u
+     WHERE u.id = d.renal_unit ) AS unitcode,
+  d.nhs_no AS nhsno,
+  ( SELECT DISTINCT um.specialty_id
+      FROM usermapping um
+     WHERE um.nhsno = d.nhs_no
        AND um.username NOT LIKE '%-GP%') AS specialty_id
 FROM tbl_demographics d
-WHERE d.renal_unit_2 IS NOT NULL;
-
-
+WHERE NOT EXISTS (
+                      SELECT d.*
+                        FROM tbl_demographics d LEFT OUTER JOIN unit u ON d.renal_unit = u.id, usermapping ump
+                        WHERE d.nhs_no = ump.nhsno
+                          AND u.unitcode = ump.unitcode
+                    );
