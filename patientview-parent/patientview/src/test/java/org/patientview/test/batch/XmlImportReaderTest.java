7ae931439cb0d10ac7334edab38327078d44e0f0
deniz@solidstategroup.com <deniz@solidstategroup.com>
2013-09-03 17:18:22 +0100

    Tidied up Import test

--- patientview-parent/patientview/src/test/java/org/patientview/test/batch/XmlImportReaderTest.java
+++ patientview-parent/patientview/src/test/java/org/patientview/test/batch/XmlImportReaderTest.java
@@ -7,12 +7,21 @@ import org.patientview.ibd.model.MyIbd;
 import org.patientview.ibd.model.Procedure;
 import org.patientview.model.Patient;
 import org.patientview.patientview.FindXmlFiles;
-import org.patientview.patientview.model.*;
+import org.patientview.patientview.model.Centre;
+import org.patientview.patientview.model.Diagnostic;
+import org.patientview.patientview.model.Letter;
+import org.patientview.patientview.model.Medicine;
+import org.patientview.patientview.model.TestResult;
+import org.patientview.patientview.model.UktStatus;
 import org.patientview.repository.PatientDao;
-import org.patientview.service.*;
+import org.patientview.service.CentreManager;
+import org.patientview.service.DiagnosticManager;
+import org.patientview.service.LetterManager;
+import org.patientview.service.MedicineManager;
+import org.patientview.service.TestResultManager;
+import org.patientview.service.UKTransplantManager;
 import org.patientview.service.ibd.IbdManager;
 import org.patientview.test.helpers.ServiceHelpers;
-import org.patientview.test.service.BaseServiceTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ResourceUtils;
 
@@ -24,6 +33,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  *  Use the 'test/resources/rm301_1244_9876543210.xml' and "test/resources/test.uktstatus.gpg.txt" to do this test,
@@ -77,13 +87,11 @@ public class XmlImportReaderTest extends BaseBatchTest {
 
         int uktFilesSize = 0;
         int xmlFilesSize = 0;
+
         String parentDir = ResourceUtils.getFile("classpath:schedule/test-uktstatus.gpg.txt").getParent();
+
         setUktDirectory(parentDir);
         setXmlDirectory(parentDir);
-
-        Specialty specialty = serviceHelpers.createSpecialty("Specialty 1", "Specialty1", "Test description");
-        User user1 = serviceHelpers.createUserWithMapping("testuser1", "paul@test.com", "p", "Testuser1", "RM301",
-                "9876543210", specialty);
 
         File uktDir = new File(uktDirectory);
         File[] uktFiles = uktDir.listFiles(new FilenameFilter() {
@@ -96,92 +104,61 @@ public class XmlImportReaderTest extends BaseBatchTest {
             uktFilesSize = uktFiles.length;
         }
 
+        assertTrue("Can not read UKT files", uktFilesSize != 0);
+
         File[] xmlFiles = FindXmlFiles.findXmlFiles(xmlDirectory, fileEndings);
         if (xmlFiles != null) {
             xmlFilesSize = xmlFiles.length;
         }
+
+        assertTrue("Can not read XML files", xmlFilesSize != 0);
 
         xmlImportReader.setXmlDirectory(parentDir);
         xmlImportReader.setUktDirectory(parentDir);
         xmlImportReader.setUktExportDirectory(parentDir);
         xmlImportReader.refresh();
 
-        if (xmlFilesSize > 0) {
-            List<Centre> centres = centreManager.getAll();
+        List<Centre> centres = centreManager.getAll();
 
-            assertEquals("Incorrect number of centres", 1, centres.size());
-            assertEquals("Incorrect centre", "RM301", centres.get(0).getCentreCode());
+        assertEquals("Incorrect number of centres", 1, centres.size());
+        assertEquals("Incorrect centre", "RM301", centres.get(0).getCentreCode());
 
-            List<Patient> patients = patientDao.get("RM301");
+        List<Patient> patients = patientDao.get("RM301");
 
-            assertEquals("Incorrect number of patients", 1, patients.size());
-            assertEquals("Incorrect patient", "9876543210", patients.get(0).getNhsno());
+        assertEquals("Incorrect number of patients", 1, patients.size());
+        assertEquals("Incorrect patient", "9876543210", patients.get(0).getNhsno());
 
-            List<Letter> letters = letterManager.getAll();
+        List<Letter> letters = letterManager.getAll();
 
-            assertEquals("Incorrect number of letters", 2, letters.size());
+        assertEquals("Incorrect number of letters", 2, letters.size());
 
-            MyIbd myIbd = ibdManager.getMyIbd("9876543210");
-            assertNotNull("No MyIbd information was parsed", myIbd);
+        MyIbd myIbd = ibdManager.getMyIbd("9876543210");
+        assertNotNull("No MyIbd information was parsed", myIbd);
 
-            Diagnostic diagnostic = diagnosticManager.get("9876543210");
-            assertNotNull("No diagnostic information was parsed", diagnostic);
+        Diagnostic diagnostic = diagnosticManager.get("9876543210");
+        assertNotNull("No diagnostic information was parsed", diagnostic);
 
-            Procedure procedure = ibdManager.getProcedure("9876543210");
-            assertNotNull("No procedure information was parsed", procedure);
+        Procedure procedure = ibdManager.getProcedure("9876543210");
+        assertNotNull("No procedure information was parsed", procedure);
 
-            Allergy allergy = ibdManager.getAllergy("9876543210");
-            assertNotNull("No allergy information was parsed", allergy);
+        Allergy allergy = ibdManager.getAllergy("9876543210");
+        assertNotNull("No allergy information was parsed", allergy);
 
-            List<TestResult> results = testResultManager.get("9876543210", "RM301");
+        List<TestResult> results = testResultManager.get("9876543210", "RM301");
 
-            assertEquals("Incorrect number of results", 3, results.size());
+        assertEquals("Incorrect number of results", 3, results.size());
 
-            List<Medicine> medicines = medicineManager.getAll();
+        List<Medicine> medicines = medicineManager.getAll();
 
-            assertEquals("Incorrect number of medicines", 0, medicines.size());
-        } else {
-            List<Centre> centres = centreManager.getAll();
-
-            assertEquals("Incorrect number of centres", 0, centres.size());
-
-            List<Patient> patients = patientDao.get("RM301");
-
-            assertEquals("Incorrect number of patients", 0, patients.size());
-
-            List<Letter> letters = letterManager.getAll();
-
-            assertEquals("Incorrect number of letters", 0, letters.size());
-
-            MyIbd myIbd = ibdManager.getMyIbd("9876543210");
-            assertNull("No MyIbd information was parsed", myIbd);
-
-            Diagnostic diagnostic = diagnosticManager.get("9876543210");
-            assertNull("No diagnostic information was parsed", diagnostic);
-
-            Procedure procedure = ibdManager.getProcedure("9876543210");
-            assertNull("No procedure information was parsed", procedure);
-
-            Allergy allergy = ibdManager.getAllergy("9876543210");
-            assertNull("No allergy information was parsed", allergy);
-
-            List<TestResult> results = testResultManager.get("9876543210", "RM301");
-
-            assertEquals("Incorrect number of results", 0, results.size());
-
-            List<Medicine> medicines = medicineManager.getAll();
-
-            assertEquals("Incorrect number of medicines", 0, medicines.size());
-        }
+        assertEquals("Incorrect number of medicines", 0, medicines.size());
 
         UktStatus uktStatus = ukTransplantManager.getUktStatus("9876543210");
+
         if (uktFilesSize > 0) {
             assertNotNull("UktStatus not be saved", uktStatus);
         } else {
             assertNull("Wrong entity exists.", uktStatus);
         }
-
-
     }
 
     public void setXmlDirectory(String xmlDirectory) {
