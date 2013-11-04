359a11ec182619a4f6ddcab40922ccd8efd892dd
Paul Chenery <paul@solidstategroup.com>
2013-11-04 14:55:26 +0000

    hot fix to change medications label, and add char length restrictions to text inputs

--- patientview-parent/radar/src/main/java/org/patientview/radar/web/pages/patient/hnf1b/HNF1BPatientPage.java
+++ patientview-parent/radar/src/main/java/org/patientview/radar/web/pages/patient/hnf1b/HNF1BPatientPage.java
@@ -1,16 +1,5 @@
 package org.patientview.radar.web.pages.patient.hnf1b;
 
-import org.patientview.radar.model.Demographics;
-import org.patientview.radar.model.generic.AddPatientModel;
-import org.patientview.radar.model.user.User;
-import org.patientview.radar.service.DemographicsManager;
-import org.patientview.radar.web.behaviours.RadarBehaviourFactory;
-import org.patientview.radar.web.pages.BasePage;
-import org.patientview.radar.web.panels.GeneticsPanel;
-import org.patientview.radar.web.panels.generic.GenericDemographicsPanel;
-import org.patientview.radar.web.panels.generic.MedicalResultsPanel;
-import org.patientview.radar.web.panels.hnf1b.HNF1BMiscPanel;
-import org.patientview.radar.web.visitors.PatientFormVisitor;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
@@ -25,6 +14,17 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.patientview.radar.model.Demographics;
+import org.patientview.radar.model.generic.AddPatientModel;
+import org.patientview.radar.model.user.User;
+import org.patientview.radar.service.DemographicsManager;
+import org.patientview.radar.web.behaviours.RadarBehaviourFactory;
+import org.patientview.radar.web.pages.BasePage;
+import org.patientview.radar.web.panels.NonAlportGeneticsPanel;
+import org.patientview.radar.web.panels.generic.GenericDemographicsPanel;
+import org.patientview.radar.web.panels.generic.MedicalResultsPanel;
+import org.patientview.radar.web.panels.hnf1b.HNF1BMiscPanel;
+import org.patientview.radar.web.visitors.PatientFormVisitor;
 
 @AuthorizeInstantiation({User.ROLE_PROFESSIONAL, User.ROLE_SUPER_USER})
 public class HNF1BPatientPage extends BasePage {
@@ -58,7 +58,7 @@ public class HNF1BPatientPage extends BasePage {
 
     // The panels we are using
     private GenericDemographicsPanel genericDemographicsPanel;
-    private GeneticsPanel geneticsPanel;
+    private NonAlportGeneticsPanel geneticsPanel;
     private HNF1BMiscPanel hnf1BMiscPanel;
     private MedicalResultsPanel medicalResultsPanel;
 
@@ -91,7 +91,7 @@ public class HNF1BPatientPage extends BasePage {
         };
         add(genericDemographicsPanel);
 
-        geneticsPanel = new GeneticsPanel("geneticsPanel", demographics) {
+        geneticsPanel = new NonAlportGeneticsPanel("geneticsPanel", demographics) {
             @Override
             public boolean isVisible() {
                 return currentTab.equals(Tab.GENETICS);
