f2d1d8c19dec47d7ed496b8ee105e7eefcb530d8
jameseaton <james@solidstategroup.com>
2013-11-19 13:24:24 +0000

    Fix to view linked records in existing patients page - Trello 174

--- patientview-parent/common/src/main/resources/sql/features/26/26.sql
+++ patientview-parent/common/src/main/resources/sql/features/26/26.sql
@@ -1,4 +1,9 @@
 /**
     Add a date column in unit table.
  */
- ALTER TABLE unit ADD COLUMN `lastImportDate` datetime DEFAULT NULL
+ ALTER TABLE unit ADD COLUMN `lastImportDate` datetime DEFAULT NULL;
+
+ /**
+    Create the link table for patient->patient
+ */
+ CREATE TABLE rdr_patient_linkage (id BIGINT PRIMARY KEY AUTO_INCREMENT, source_nhsno VARCHAR(20), source_unitcode VARCHAR(20), dest_nhsno VARCHAR(20), dest_unitcode VARCHAR(20));
