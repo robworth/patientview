03956e676c4bb0c6345e4aadb3ed3c0ea478317f
Paul Chenery <paul@solidstategroup.com>
2013-12-05 18:11:46 +0000

    further corrections to patient merge data scripts

--- patientview-parent/common/src/main/resources/sql/features/21/data.sql
+++ patientview-parent/common/src/main/resources/sql/features/21/data.sql
@@ -1,3 +1,8 @@
+ALTER TABLE patient drop UNIQUE KEY `nhsno`;
+
+ALTER TABLE patient ADD UNIQUE KEY `nhsno-unitcode-sourceType` (`nhsno`,`unitcode`, `sourceType`);
+
+
 /**
      Inserts data from tbl_demograpics to patient
      -- there are 64 patients that exist in PV and Radar which the same nhsno and unit, we will not merge these
@@ -10,30 +15,21 @@ INSERT INTO patient
 consNeph, STATUS, emailAddress, rrtModality, genericDiagnosis, dateOfGenericDiagnosis, otherClinicianAndContactInfo,
 comments, republicOfIrelandId, isleOfManId, channelIslandsId, indiaId, generic, NHSNO, HOSPITALNUMBER, SURNAME,
 FORENAME, DATEOFBIRTH, ADDRESS1, ADDRESS2, ADDRESS3, ADDRESS4, POSTCODE, TELEPHONE1, TELEPHONE2, MOBILE, unitcode,
-sourceType)
+sourceType, sex)
 SELECT
 d.RADAR_NO, d.RR_NO, d.DATE_REG, d.NHS_NO_TYPE, d.UKT_NO, d.SNAME_ALIAS, d.AGE, d.ETHNIC_GP, d.POSTCODE_OLD,
 d.CONSENT, d.DATE_BAPN_REG, d.CONS_NEPH, d.STATUS, d.emailAddress, d.RRT_modality, d.genericDiagnosis,
 d.dateOfGenericDiagnosis, d.otherClinicianAndContactInfo, d.comments, d.republicOfIrelandId, d.isleOfManId,
 d.channelIslandsId, d.indiaId, d.generic, d.NHS_NO, d.HOSP_NO, d.SNAME, d.FNAME, d.DOB, d.ADD1, d.ADD2, d.ADD3,
-d.ADD4, d.POSTCODE, d.PHONE1, d.PHONE2, d.MOBILE, d.RDG, 'Radar'
-FROM tbl_demographics d;
-
-
-/**
-    Update the sex
- */
-
-UPDATE patient p INNER JOIN
-    (
-      SELECT s.sType,d.*
-        FROM tbl_sex s, tbl_demographics d
-        WHERE s.sID = d.sex
-    ) dData ON p.nhsno = dData.nhs_no AND p.unitcode = dData.RDG
-SET p.sex = dData.sType;
+d.ADD4, d.POSTCODE, d.PHONE1, d.PHONE2, d.MOBILE, u.unitcode, 'Radar', s.sType
+FROM tbl_demographics d, unit u, tbl_sex s
+WHERE
+d.renal_unit = u.id
+AND
+d.sex = s.sId;
 
 /**
-    NOTE - RUN THE UserUpgradeManager.addUserForAllRadarPatients before this part of script
+    todo NOTE - RUN THE UserUpgradeManager.addUserForAllRadarPatients before this part of script
     (Created 279 new users, and 372 new mappings for 544 users)
 
     Create a user mapping for the disease group
@@ -43,22 +39,16 @@ SET p.sex = dData.sType;
  */
 
 INSERT INTO usermapping (username, unitcode, nhsno, specialty_id)
-SELECT
-u.username AS username,
-unitcode,
-p.nhsno,
-1 AS specialty_id
-FROM patient p, tbl_patient_users pu, rdr_user_mapping m, USER u
-WHERE
-p.radarNo = pu.RADAR_NO
-AND
-pu.pID = m.radarUserId
-AND
-m.userId = u.id
-AND
-p.sourceType = 'Radar'
-AND
-m.role = 'ROLE_PATIENT';
+SELECT user.username AS username,
+  d.RDG AS unitcode,
+  d.nhs_no AS nhsno,
+  1 AS specialty_id
+FROM tbl_demographics d, tbl_patient_users, rdr_user_mapping, user
+WHERE d.RADAR_NO = tbl_patient_users.RADAR_NO
+  AND tbl_patient_users.pID = rdr_user_mapping.radarUserId
+  AND rdr_user_mapping.userId = user.id
+  AND role = 'ROLE_PATIENT';
+
 /**
     Create a user mapping for the source data entering user's unit
     Only do this if it does not already exist
--- patientview-parent/common/src/main/resources/sql/features/26/26.sql
+++ patientview-parent/common/src/main/resources/sql/features/26/26.sql
@@ -3,11 +3,6 @@
  */
  ALTER TABLE unit ADD COLUMN `lastImportDate` datetime DEFAULT NULL;
 
- /**
-    Create the link table for patient->patient
- */
- CREATE TABLE rdr_patient_linkage (id BIGINT PRIMARY KEY AUTO_INCREMENT, source_nhsno VARCHAR(20), source_unitcode VARCHAR(20), dest_nhsno VARCHAR(20), dest_unitcode VARCHAR(20));
-
  ALTER TABLE pv_patientjoin_request CHANGE email email VARCHAR(255) NULL;
 
  ALTER TABLE pv_patientjoin_request MODIFY COLUMN dateofbirth DATETIME NOT NULL;  -- check dateofbirth has valid dates
