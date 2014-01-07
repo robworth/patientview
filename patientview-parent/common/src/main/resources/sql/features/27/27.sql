0db91b26e249d03e305c10476d7fb71b7103ee18
jameseaton <james@solidstategroup.com>
2014-01-07 13:48:04 +0000

    RPV-153 Rectified DB script to keep existing dates

--- patientview-parent/common/src/main/resources/sql/features/27/27.sql
+++ patientview-parent/common/src/main/resources/sql/features/27/27.sql
@@ -5,5 +5,11 @@ UPDATE patient SET patientLinkId = NULL WHERE patientlinkId = 0;
 # Change the table so it used constraints
 ALTER TABLE patient ENGINE=InnoDB;
 
+ALTER TABLE PATIENT ADD (lastdatadate DATE);
+
+UPDATE patient p
+SET lastdatadate = (SELECT MAX(lastdatadate) FROM pv_user_log l WHERE p.nhsno = l.nhsno AND p.unitcode = l.unitcode);
+
+
 DROP TABLE pv_user_log;
 
