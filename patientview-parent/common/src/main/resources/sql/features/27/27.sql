15149cf5cce882b06a070575ee8eb07145c982b8
jameseaton <james@solidstategroup.com>
2014-01-08 14:00:28 +0000

    RPV-147 Drop bad user mappings

--- patientview-parent/common/src/main/resources/sql/features/27/27.sql
+++ patientview-parent/common/src/main/resources/sql/features/27/27.sql
@@ -1,8 +1,21 @@
-# Lets remove the back patientLinkId
+# Removing the bad patient mappings
+
+DELETE   usm
+FROM     specialtyuserrole spu
+,        USER usr
+,        usermapping usm
+WHERE    spu.role = 'unitadmin'
+AND      spu.id = usr.id
+AND      usr.username = usm.username
+AND      unitcode = 'PATIENT'
+AND      nhsno IS NULL;
+
+# Lets remove the bad patientLinkIds
 
 UPDATE patient SET patientLinkId = NULL WHERE patientlinkId = 0;
 
 # Change the table so it used constraints
 ALTER TABLE patient ENGINE=InnoDB;
+
 
 
