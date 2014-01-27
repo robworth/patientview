2318b933e50b58a15473329ac1ff1a1e3054ab5a
jameseaton <james@solidstategroup.com>
2014-01-27 11:55:59 +0000

    Trello 144 - Forgotten password page

--- patientview-parent/common/src/main/resources/sql/features/25/25.sql
+++ patientview-parent/common/src/main/resources/sql/features/25/25.sql
@@ -1,17 +1,4 @@
 /**
-    Add 2 columns in user table.
-    remove a column in user table.
- */
-ALTER TABLE user ADD COLUMN `firstName` VARCHAR(100)  NULL;
-ALTER TABLE user ADD COLUMN `lastName` VARCHAR(100)  NULL;
-
-UPDATE user
-SET firstName = SUBSTRING_INDEX(  NAME ,' ', 1),
-lastName = SUBSTRING_INDEX(  NAME ,' ', -1);
-
-ALTER TABLE `user` DROP COLUMN `name`;
-=======
-    /**
     alter the columns in the tables to DATETIME
      */
     /**
