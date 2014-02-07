d4571f09e84fe7c1a7acc8b2c756181acb31c65e
jameseaton <james@solidstategroup.com>
2014-02-07 11:57:25 +0000

    Complication fix

--- patientview-parent/patientview/src/main/java/org/patientview/patientview/model/TestResultWithUnitShortname.java
+++ patientview-parent/patientview/src/main/java/org/patientview/patientview/model/TestResultWithUnitShortname.java
@@ -34,7 +34,7 @@ public class TestResultWithUnitShortname extends TestResult {
 
     public TestResultWithUnitShortname(String nhsno, String unitcode, Calendar datestamp, String testcode,
                                        String value, String shortname) {
-        super(nhsno, unitcode, datestamp, testcode, value, null);
+        super(nhsno, unitcode, datestamp, testcode, value);
         this.shortname = shortname;
     }
 
